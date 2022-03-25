db08c9aa35a5d6135dac9b3666a5267cf838950e
Edie Lemoine <edie@myparcel.nl>
2022-03-25 16:13:27 +0100

    build: update release flow

--- release.config.js
+++ release.config.js
@@ -1,10 +1,11 @@
 const mainConfig = require('@myparcel/semantic-release-config/composer');
-const { addGitHubPlugin } = require('@myparcel/semantic-release-config/src/plugins');
+const { addGitHubPlugin, addGitPlugin } = require('@myparcel/semantic-release-config/src/plugins');
 
 module.exports = {
   'extends': '@myparcel/semantic-release-config/composer',
   'plugins': [
     ...mainConfig.plugins,
     addGitHubPlugin(),
+    addGitPlugin(),
   ],
 };
