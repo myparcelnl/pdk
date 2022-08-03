ef8dbfb35333201905e6855e88598c7e7577c25b
Thijmen Wijers <thijmen.wijers@myparcel.nl>
2022-08-03 17:18:56 +0200

    wip

--- src/Form/Helpers/FormBuilder.php
+++ src/Form/Helpers/FormBuilder.php
@@ -4,18 +4,42 @@ declare(strict_types=1);
 
 namespace MyParcelNL\Pdk\Form\Helpers;
 
-use MyParcelNL\Pdk\Form\Model\Input\Select\CountrySelect;
-use MyParcelNL\Pdk\Form\Model\Input\SelectInput;
-use MyParcelNL\Pdk\Form\Model\Input\TextInput;
+use MyParcelNL\Pdk\Settings\Model\View\CheckoutSettingsView;
 use MyParcelNL\Pdk\Settings\Model\View\CustomsSettingsView;
+use MyParcelNL\Pdk\Settings\Model\View\LabelSettingsView;
+use MyParcelNL\Pdk\Settings\Model\View\OrderSettingsView;
 
 class FormBuilder
 {
     /**
      * @return \MyParcelNL\Pdk\Settings\Model\View\CustomsSettingsView
      */
-    public function getCustomsView(): CustomsSettingsView
+    public function getCustomsSettingsView(): CustomsSettingsView
     {
         return new CustomsSettingsView();
+    }
+
+    /**
+     * @return \MyParcelNL\Pdk\Settings\Model\View\LabelSettingsView
+     */
+    public function getLabelSettingsView(): LabelSettingsView
+    {
+        return new LabelSettingsView();
+    }
+
+    /**
+     * @return \MyParcelNL\Pdk\Settings\Model\View\OrderSettingsView
+     */
+    public function getOrderSettingsView(): OrderSettingsView
+    {
+        return new OrderSettingsView();
+    }
+
+    /**
+     * @return \MyParcelNL\Pdk\Settings\Model\View\CheckoutSettingsView
+     */
+    public function getCheckoutSettingsView(): CheckoutSettingsView
+    {
+        return new CheckoutSettingsView();
     }
 }
--- src/Settings/Model/View/CheckoutSettingsView.php
+++ src/Settings/Model/View/CheckoutSettingsView.php
@@ -9,19 +9,35 @@ use MyParcelNL\Pdk\Form\Model\Input\TextInput;
 
 /**
  * @property \MyParcelNL\Pdk\Form\Model\Input\TextInput $deliveryTitle
+ * @property \MyParcelNL\Pdk\Form\Model\Input\TextInput $standardDeliveryTitle
+ * @property \MyParcelNL\Pdk\Form\Model\Input\TextInput $morningDeliveryTitle
  */
 class CheckoutSettingsView extends Model
 {
     public function __construct(array $data = null)
     {
-        $this->attributes['deliveryTitle'] = [
+        $this->attributes['deliveryTitle']         = [
             'type'  => 'text',
             'name'  => 'deliveryTitle',
             'label' => 'Delivery Title',
             'desc'  => 'Title of the delivery option.',
         ];
+        $this->attributes['standardDeliveryTitle'] = [
+            'type'  => 'text',
+            'name'  => 'standardDeliveryTitle',
+            'label' => 'Standard delivery title',
+            'desc'  => 'When there is no title, the delivery time will automatically be visible',
+        ];
+        $this->attributes['morningDeliveryTitle']  = [
+            'type'  => 'text',
+            'name'  => 'morningDeliveryTitle',
+            'label' => 'Morning delivery title',
+            'desc'  => 'When there is no title, delivery time will automatically be visible',
+        ];
 
-        $this->casts['deliveryTitle'] = TextInput::class;
+        $this->casts['deliveryTitle']         = TextInput::class;
+        $this->casts['standardDeliveryTitle'] = TextInput::class;
+        $this->casts['morningDeliveryTitle']  = TextInput::class;
 
         parent::__construct($data);
     }
