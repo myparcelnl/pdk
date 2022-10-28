5e301203eb6f53107cb0e02154ecd6810f6fd561
Joeri van Veen <joeri@myparcel.nl>
2022-10-28 10:01:36 +0200

    wip (bpost)

--- config/validation/belgie/order.php
+++ config/validation/belgie/order.php
@@ -9,6 +9,54 @@ use MyParcelNL\Pdk\Shipment\Model\DeliveryOptions;
 return [
     'carrier' => [
         [
+            'id'           => CarrierOptions::CARRIER_BPOST_ID,
+            'name'         => CarrierOptions::CARRIER_BPOST_NAME,
+            'human'        => 'Bpost',
+            'schema'       => 'order/bpost/base',
+            'shippingZone' => [
+                [
+                    'cc'   => CountryService::CC_BE,
+                    'note' => 'Is validated using base',
+                ],
+                [
+                    'cc'          => CountryService::CC_NL,
+                    'packageType' => [
+                        [
+                            'id'     => DeliveryOptions::PACKAGE_TYPE_PACKAGE_ID,
+                            'name'   => DeliveryOptions::PACKAGE_TYPE_PACKAGE_NAME,
+                            'schema' => 'order/bpost/eu_package',
+                        ],
+                    ],
+                ],
+                [
+                    'cc'          => CountryService::ZONE_EU,
+                    'packageType' => [
+                        [
+                            'id'     => DeliveryOptions::PACKAGE_TYPE_PACKAGE_ID,
+                            'name'   => DeliveryOptions::PACKAGE_TYPE_PACKAGE_NAME,
+                            'schema' => 'order/bpost/eu_package',
+                        ],
+                    ],
+                ],
+                [
+                    'cc'          => CountryService::ZONE_ROW,
+                    'packageType' => [
+                        [
+                            'id'     => DeliveryOptions::PACKAGE_TYPE_PACKAGE_ID,
+                            'name'   => DeliveryOptions::PACKAGE_TYPE_PACKAGE_NAME,
+                            'schema' => 'order/bpost/row_package',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+        [
+            'id'     => CarrierOptions::CARRIER_DPD_ID,
+            'name'   => CarrierOptions::CARRIER_DPD_NAME,
+            'human'  => 'DPD',
+            'schema' => 'order/dpd/base',
+        ],
+        [
             'id'           => CarrierOptions::CARRIER_POSTNL_ID,
             'name'         => CarrierOptions::CARRIER_POSTNL_NAME,
             'human'        => 'PostNL',
@@ -35,9 +83,9 @@ return [
                     'cc'          => CountryService::CC_NL,
                     'packageType' => [
                         [
-                            'id'           => DeliveryOptions::PACKAGE_TYPE_PACKAGE_ID,
-                            'name'         => DeliveryOptions::PACKAGE_TYPE_PACKAGE_NAME,
-                            'schema'       => 'order/postnl/nl_package',
+                            'id'     => DeliveryOptions::PACKAGE_TYPE_PACKAGE_ID,
+                            'name'   => DeliveryOptions::PACKAGE_TYPE_PACKAGE_NAME,
+                            'schema' => 'order/postnl/nl_package',
                         ],
                     ],
                 ],
@@ -85,88 +133,6 @@ return [
                     ],
                 ],
             ],
-        ],
-        [
-            'id'           => CarrierOptions::CARRIER_BPOST_ID,
-            'name'         => CarrierOptions::CARRIER_BPOST_NAME,
-            'human'        => 'Bpost',
-            'schema'       => 'order/bpost/base',
-            'shippingZone' => [
-                [
-                    'cc'          => CountryService::CC_BE,
-                    'packageType' => [
-                        [
-                            'id'           => DeliveryOptions::PACKAGE_TYPE_PACKAGE_ID,
-                            'name'         => DeliveryOptions::PACKAGE_TYPE_PACKAGE_NAME,
-                            'schema'       => 'order/bpost/be_package',
-                            'deliveryType' => [
-                                [
-                                    'id'   => DeliveryOptions::DELIVERY_TYPE_STANDARD_ID,
-                                    'name' => DeliveryOptions::DELIVERY_TYPE_STANDARD_NAME,
-                                ],
-                                [
-                                    'id'   => DeliveryOptions::DELIVERY_TYPE_PICKUP_ID,
-                                    'name' => DeliveryOptions::DELIVERY_TYPE_PICKUP_NAME,
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'cc'          => CountryService::CC_NL,
-                    'packageType' => [
-                        [
-                            'id'           => DeliveryOptions::PACKAGE_TYPE_PACKAGE_ID,
-                            'name'         => DeliveryOptions::PACKAGE_TYPE_PACKAGE_NAME,
-                            'schema'       => 'order/bpost/nl_package',
-                            'deliveryType' => [
-                                [
-                                    'id'   => DeliveryOptions::DELIVERY_TYPE_STANDARD_ID,
-                                    'name' => DeliveryOptions::DELIVERY_TYPE_STANDARD_NAME,
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'cc'          => CountryService::ZONE_EU,
-                    'packageType' => [
-                        [
-                            'id'           => DeliveryOptions::PACKAGE_TYPE_PACKAGE_ID,
-                            'name'         => DeliveryOptions::PACKAGE_TYPE_PACKAGE_NAME,
-                            'schema'       => 'order/bpost/eu_package',
-                            'deliveryType' => [
-                                [
-                                    'id'   => DeliveryOptions::DELIVERY_TYPE_STANDARD_ID,
-                                    'name' => DeliveryOptions::DELIVERY_TYPE_STANDARD_NAME,
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'cc'          => CountryService::ZONE_ROW,
-                    'packageType' => [
-                        [
-                            'id'           => DeliveryOptions::PACKAGE_TYPE_PACKAGE_ID,
-                            'name'         => DeliveryOptions::PACKAGE_TYPE_PACKAGE_NAME,
-                            'schema'       => 'order/bpost/row_package',
-                            'deliveryType' => [
-                                [
-                                    'id'   => DeliveryOptions::DELIVERY_TYPE_STANDARD_ID,
-                                    'name' => DeliveryOptions::DELIVERY_TYPE_STANDARD_NAME,
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-            ],
-        ],
-        [
-            'id'           => CarrierOptions::CARRIER_DPD_ID,
-            'name'         => CarrierOptions::CARRIER_DPD_NAME,
-            'human'        => 'DPD',
-            'schema'       => 'order/dpd/base',
         ],
     ],
 ];
