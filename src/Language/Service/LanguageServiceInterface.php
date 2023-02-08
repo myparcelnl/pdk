5ff0e7b6ce172ef6c40e292f2aae872c08fb083a
Edie Lemoine <edie@myparcel.nl>
2023-02-08 17:16:33 +0100

    feat(language): add more methods
    - add getIso2
    - add translateArray
    - add hasTranslation

--- src/Language/Service/LanguageServiceInterface.php
+++ src/Language/Service/LanguageServiceInterface.php
@@ -7,11 +7,24 @@ namespace MyParcelNL\Pdk\Language\Service;
 interface LanguageServiceInterface
 {
     /**
-     * @return string
+     * Get iso 2 language code.
+     *
+     * @example en
+     * @example nl
+     */
+    public function getIso2(?string $language = null): string;
+
+    /**
+     * Returns the IETF language tag of the current language.
+     *
+     * @example en-US
+     * @example nl-NL
      */
     public function getLanguage(): string;
 
     /**
+     * Get an array of translations for the given language. If no language is given, the current language is used.
+     *
      * @param  null|string $language
      *
      * @return array<string, string>
@@ -19,10 +32,19 @@ interface LanguageServiceInterface
     public function getTranslations(?string $language = null): array;
 
     /**
-     * @param  string      $key
-     * @param  null|string $language
-     *
-     * @return string
+     * Check if a translation exists for the given key in the given language. If no language is given, the current
+     * language is used.
+     */
+    public function hasTranslation(string $key, ?string $language = null): bool;
+
+    /**
+     * Translate a string by key in the given language. Falls back to the current language if no language is given.
      */
     public function translate(string $key, ?string $language = null): string;
+
+    /**
+     * Translate an array of strings by key in the given language. Falls back to the current language if no language is
+     * given.
+     */
+    public function translateArray(array $array, ?string $language = null): array;
 }
