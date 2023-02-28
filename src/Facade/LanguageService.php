b499b44fa4ce6b74346f49ea5f2f5ace4e742767
Edie Lemoine <edie@myparcel.nl>
2023-02-28 16:18:55 +0100

    feat(language): add more methods (#85)
    - add getIso2
    - add translateArray
    - add hasTranslation

--- src/Facade/LanguageService.php
+++ src/Facade/LanguageService.php
@@ -8,9 +8,12 @@ use MyParcelNL\Pdk\Base\Facade;
 use MyParcelNL\Pdk\Language\Service\LanguageServiceInterface;
 
 /**
+ * @method static string getIso2(string $language = null)
  * @method static string getLanguage()
  * @method static array<string, string> getTranslations(string $language = null)
  * @method static string translate(string $string, string $language = null)
+ * @method static array translateArray(array $array, string $language = null)
+ * @method static string hasTranslation(string $string, string $language = null)
  * @implements \MyParcelNL\Pdk\Language\Service\LanguageServiceInterface
  */
 class LanguageService extends Facade
--- tests/Unit/Language/Service/AbstractLanguageServiceTest.php
+++ tests/Unit/Language/Service/AbstractLanguageServiceTest.php
@@ -3,27 +3,30 @@
 
 declare(strict_types=1);
 
-use MyParcelNL\Pdk\Base\Factory\PdkFactory;
 use MyParcelNL\Pdk\Facade\LanguageService;
 use MyParcelNL\Pdk\Language\Service\LanguageServiceInterface;
 use MyParcelNL\Pdk\Tests\Bootstrap\MockAbstractLanguageService;
-use MyParcelNL\Pdk\Tests\Bootstrap\MockPdkConfig;
+use MyParcelNL\Pdk\Tests\Uses\UsesEachMockPdkInstance;
 use function DI\autowire;
+use function MyParcelNL\Pdk\Tests\usesShared;
 
-beforeEach(function () {
-    PdkFactory::create(
-        MockPdkConfig::create([
+usesShared(
+    new UsesEachMockPdkInstance([
             LanguageServiceInterface::class => autowire(MockAbstractLanguageService::class),
-        ])
-    );
-});
+        ]
+    )
+);
 
 dataset('languages', [
-    ['language' => 'en', 'translation' => 'Send help'],
-    ['language' => 'nl', 'translation' => 'Stuur hulp'],
+    ['language' => 'en-GB', 'translation' => 'Send help', 'iso2' => 'en'],
+    ['language' => 'nl-NL', 'translation' => 'Stuur hulp', 'iso2' => 'nl'],
     // Use current language, which is 'en' in MockAbstractLanguageService.
-    ['language' => null, 'translation' => 'Send help'],
+    ['language' => null, 'translation' => 'Send help', 'iso2' => 'en'],
 ]);
+
+it('gets current language', function () {
+    expect(LanguageService::getLanguage())->toBe('en-GB');
+});
 
 it('loads translations from file', function (?string $language) {
     expect(LanguageService::getTranslations($language))->toBeArray();
@@ -43,4 +46,68 @@ it('translates strings in given language', function (?string $language, string $
 
 it('ignores missing strings', function () {
     expect(LanguageService::translate('help_is_coming'))->toBe('help_is_coming');
+});
+
+it('checks if a translation exists', function () {
+    expect(LanguageService::hasTranslation('send_help'))
+        ->toBeTrue()
+        ->and(LanguageService::hasTranslation('help_is_coming'))
+        ->toBeFalse();
+});
+
+it('converts to iso2 language code', function (?string $language, string $translation, string $iso2) {
+    expect(LanguageService::getIso2($language))->toBe($iso2);
+})->with('languages');
+
+it('translates indexed arrays', function () {
+    $result = LanguageService::translateArray(['send_help', 'help_is_coming']);
+    expect($result)
+        ->toBeArray()
+        ->and($result)
+        ->toBe([
+            'send_help'      => 'Send help',
+            'help_is_coming' => 'help_is_coming',
+        ]);
+});
+
+it('translates associative arrays', function () {
+    $result = LanguageService::translateArray([
+        'string_1' => 'send_help',
+        'string_2' => 'help_is_coming',
+        'string_3' => 'i_am_trapped',
+    ]);
+
+    expect($result)
+        ->toBeArray()
+        ->and($result)
+        ->toBe([
+            'string_1' => 'Send help',
+            'string_2' => 'help_is_coming',
+            'string_3' => 'I am stuck',
+        ]);
+});
+
+it('translates nested associative arrays', function () {
+    $result = LanguageService::translateArray([
+        'string_1'  => 'send_help',
+        'string_2'  => 'help_is_coming',
+        'send_help' => [
+            'string_3'       => 'send_help',
+            'help_is_coming' => 'help_is_coming',
+            'i_am_trapped'   => 'i_am_trapped',
+        ],
+    ]);
+
+    expect($result)
+        ->toBeArray()
+        ->and($result)
+        ->toBe([
+            'string_1'  => 'Send help',
+            'string_2'  => 'help_is_coming',
+            'send_help' => [
+                'string_3'       => 'Send help',
+                'help_is_coming' => 'help_is_coming',
+                'i_am_trapped'   => 'I am stuck',
+            ],
+        ]);
 });
