2c15d9d25d7c2bec9f956e4757eb2e8e94c6199a
Edie Lemoine <edie@myparcel.nl>
2023-03-16 18:20:43 +0100

    perf(shipments): do not return deleted shipments in order data context

--- src/Plugin/Model/Context/OrderDataContext.php
+++ src/Plugin/Model/Context/OrderDataContext.php
@@ -4,19 +4,40 @@ declare(strict_types=1);
 
 namespace MyParcelNL\Pdk\Plugin\Model\Context;
 
+use MyParcelNL\Pdk\Base\Support\Arr;
 use MyParcelNL\Pdk\Plugin\Context;
 use MyParcelNL\Pdk\Plugin\Model\PdkOrder;
 
 /**
- * @property null|string                                                 $externalIdentifier
- * @property null|\MyParcelNL\Pdk\Shipment\Model\CustomsDeclaration      $customsDeclaration
- * @property null|\MyParcelNL\Pdk\Shipment\Model\DeliveryOptions         $deliveryOptions
- * @property null|\MyParcelNL\Pdk\Base\Model\ContactDetails              $recipient
- * @property null|\MyParcelNL\Pdk\Base\Model\ContactDetails              $sender
- * @property null|\MyParcelNL\Pdk\Shipment\Collection\ShipmentCollection $shipments
- * @property null|\MyParcelNL\Pdk\Shipment\Model\Label                   $label
+ * @property string                                                 $externalIdentifier
+ * @property null|\MyParcelNL\Pdk\Shipment\Model\CustomsDeclaration $customsDeclaration
+ * @property \MyParcelNL\Pdk\Shipment\Model\DeliveryOptions         $deliveryOptions
+ * @property \MyParcelNL\Pdk\Base\Model\ContactDetails              $recipient
+ * @property null|\MyParcelNL\Pdk\Base\Model\ContactDetails         $sender
+ * @property \MyParcelNL\Pdk\Shipment\Collection\ShipmentCollection $shipments
  */
 class OrderDataContext extends PdkOrder
 {
     public const ID = Context::ID_ORDER_DATA;
+
+    /**
+     * Remove deleted shipments from the array.
+     *
+     * @param  null|int $flags
+     *
+     * @return array
+     * @throws \MyParcelNL\Pdk\Base\Exception\InvalidCastException
+     */
+    public function toArray(?int $flags = null): array
+    {
+        $array = parent::toArray($flags);
+
+        return array_replace($array, [
+            'shipments' => array_values(
+                Arr::where($array['shipments'], static function (array $shipment) {
+                    return ! $shipment['deleted'];
+                })
+            ),
+        ]);
+    }
 }
