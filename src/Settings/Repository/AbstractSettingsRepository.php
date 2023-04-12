73db213e0d036a61322b28ba2d562a7e80f9dd78
Edie Lemoine <edie@myparcel.nl>
2023-04-12 12:52:26 +0200

    style: fix formatting

--- src/Settings/Repository/AbstractSettingsRepository.php
+++ src/Settings/Repository/AbstractSettingsRepository.php
@@ -15,21 +15,6 @@ use MyParcelNL\Pdk\Settings\Model\Settings;
 abstract class AbstractSettingsRepository extends ApiRepository implements SettingsRepositoryInterface
 {
     /**
-     * @param  string $namespace
-     *
-     * @return array|\MyParcelNL\Pdk\Settings\Model\AbstractSettingsModel
-     */
-    abstract public function getGroup(string $namespace);
-
-    /**
-     * @param  string $key
-     * @param  mixed  $value
-     *
-     * @return void
-     */
-    abstract protected function store(string $key, $value): void;
-
-    /**
      * @return \MyParcelNL\Pdk\Settings\Model\Settings
      * @throws \MyParcelNL\Pdk\Base\Exception\InvalidCastException
      */
@@ -75,6 +60,13 @@ abstract class AbstractSettingsRepository extends ApiRepository implements Setti
     }
 
     /**
+     * @param  string $namespace
+     *
+     * @return array|\MyParcelNL\Pdk\Settings\Model\AbstractSettingsModel
+     */
+    abstract public function getGroup(string $namespace);
+
+    /**
      * @param  AbstractSettingsModel|SettingsModelCollection $settings
      *
      * @return void
@@ -88,7 +80,7 @@ abstract class AbstractSettingsRepository extends ApiRepository implements Setti
         }
 
         /** @var array $existing */
-        $id = $settings->first()->id ?? $settings->id;
+        $id       = $settings->first()->id ?? $settings->id;
         $existing = $this->get($id) ?? [];
 
         $this->store($id, array_replace($existing, $settings->toStorableArray()));
@@ -101,6 +93,14 @@ abstract class AbstractSettingsRepository extends ApiRepository implements Setti
     {
         return 'settings_';
     }
+
+    /**
+     * @param  string $key
+     * @param  mixed  $value
+     *
+     * @return void
+     */
+    abstract protected function store(string $key, $value): void;
 
     /**
      * @param  \MyParcelNL\Pdk\Settings\Model\Settings                     $settings
