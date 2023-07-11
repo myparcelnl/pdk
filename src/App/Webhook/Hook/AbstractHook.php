12c1ddd1d3f57a5aed0eb9e8cf810deb36ebfb32
Edie Lemoine <edie@myparcel.nl>
2023-07-11 20:41:48 +0200

    feat(webhook): add logic to shipment label created and status change webhooks

--- src/App/Webhook/Hook/AbstractHook.php
+++ src/App/Webhook/Hook/AbstractHook.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace MyParcelNL\Pdk\App\Webhook\Hook;
 
 use MyParcelNL\Pdk\App\Webhook\Contract\HookInterface;
+use MyParcelNL\Pdk\Webhook\Model\WebhookSubscription;
 use RuntimeException;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -27,6 +28,31 @@ abstract class AbstractHook implements HookInterface
      */
     public function validate(Request $request): bool
     {
-        return true;
+        return $this->eventMatches($request, WebhookSubscription::SHIPMENT_STATUS_CHANGE);
+    }
+
+    /**
+     * @param  \Symfony\Component\HttpFoundation\Request $request
+     * @param  string                                    $hook
+     *
+     * @return bool
+     */
+    protected function eventMatches(Request $request, string $hook): bool
+    {
+        $content = $this->getHookBody($request);
+
+        return $request->headers['x-myparcel-hook'] === $hook && $content['event'] === $hook;
+    }
+
+    /**
+     * @param  \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return array
+     */
+    protected function getHookBody(Request $request): array
+    {
+        $body = json_decode($request->getContent(), true);
+
+        return $body['data']['hooks'][0] ?? [];
     }
 }
--- src/App/Webhook/Hook/ShipmentStatusChangeWebhook.php
+++ src/App/Webhook/Hook/ShipmentStatusChangeWebhook.php
@@ -4,6 +4,24 @@ declare(strict_types=1);
 
 namespace MyParcelNL\Pdk\App\Webhook\Hook;
 
+use MyParcelNL\Pdk\App\Api\Backend\PdkBackendActions;
+use MyParcelNL\Pdk\Facade\Actions;
+use Symfony\Component\HttpFoundation\Request;
+
 final class ShipmentStatusChangeWebhook extends AbstractHook
 {
+    /**
+     * @param  \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return void
+     */
+    public function handle(Request $request): void
+    {
+        $content = $this->getHookBody($request);
+
+        Actions::execute(PdkBackendActions::UPDATE_SHIPMENTS, [
+            'orderIds'    => [$content['shipment_reference_identifier']],
+            'shipmentIds' => [$content['shipment_id']],
+        ]);
+    }
 }
