3c02fddff2dbc07dd2a02c8d063cfbfba90e3298
markernst97 <markiervc@gmail.com>
2023-07-13 13:22:24 +0200

    feat: add order notes

--- src/App/Order/Contract/PdkOrderRepositoryInterface.php
+++ src/App/Order/Contract/PdkOrderRepositoryInterface.php
@@ -6,18 +6,25 @@ namespace MyParcelNL\Pdk\App\Order\Contract;
 
 use MyParcelNL\Pdk\App\Order\Collection\PdkOrderCollection;
 use MyParcelNL\Pdk\App\Order\Model\PdkOrder;
+use MyParcelNL\Pdk\Fulfilment\Collection\OrderNoteCollection;
 
 interface PdkOrderRepositoryInterface
 {
     /**
      * Create a new order object from input data.
+     *
+     * @param $input
+     *
+     * @return \MyParcelNL\Pdk\App\Order\Model\PdkOrder
      */
     public function get($input): PdkOrder;
 
     /**
      * Create a collection of order objects from input data
      *
-     * @param  string|string[] $orderIds - Single id, array of ids or string of semicolon-separated ids.
+     * @param $orderIds
+     *
+     * @return \MyParcelNL\Pdk\App\Order\Collection\PdkOrderCollection
      */
     public function getMany($orderIds): PdkOrderCollection;
 
@@ -26,17 +33,25 @@ interface PdkOrderRepositoryInterface
      *
      * @param  null|string $externalIdentifier
      *
-     * @return array
+     * @return \MyParcelNL\Pdk\Fulfilment\Collection\OrderNoteCollection
      */
-    public function getOrderNotes(?string $externalIdentifier): array;
+    public function getOrderNotes(?string $externalIdentifier): OrderNoteCollection;
 
     /**
      * Update order data.
+     *
+     * @param  \MyParcelNL\Pdk\App\Order\Model\PdkOrder $order
+     *
+     * @return \MyParcelNL\Pdk\App\Order\Model\PdkOrder
      */
     public function update(PdkOrder $order): PdkOrder;
 
     /**
      * Update order data in bulk.
+     *
+     * @param  \MyParcelNL\Pdk\App\Order\Collection\PdkOrderCollection $collection
+     *
+     * @return \MyParcelNL\Pdk\App\Order\Collection\PdkOrderCollection
      */
     public function updateMany(PdkOrderCollection $collection): PdkOrderCollection;
 }
