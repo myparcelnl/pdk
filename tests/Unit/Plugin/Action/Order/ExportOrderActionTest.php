797b32239b29b5aeeadb8fe00d92ae0593651b6a
Edie Lemoine <edie@myparcel.nl>
2023-07-17 17:13:24 +0200

    test: update export order action test

--- tests/Unit/Plugin/Action/Order/ExportOrderActionTest.php
+++ tests/Unit/Plugin/Action/Order/ExportOrderActionTest.php
@@ -5,12 +5,11 @@ declare(strict_types=1);
 
 namespace MyParcelNL\Pdk\Tests\Unit\Plugin\Action\Order;
 
+use Exception;
 use MyParcelNL\Pdk\Api\Contract\ApiServiceInterface;
 use MyParcelNL\Pdk\App\Api\Backend\PdkBackendActions;
 use MyParcelNL\Pdk\App\Order\Collection\PdkOrderCollection;
 use MyParcelNL\Pdk\App\Order\Contract\PdkOrderRepositoryInterface;
-use MyParcelNL\Pdk\App\Order\Model\PdkOrder;
-use MyParcelNL\Pdk\Base\Model\ContactDetails;
 use MyParcelNL\Pdk\Base\Support\Arr;
 use MyParcelNL\Pdk\Facade\Actions;
 use MyParcelNL\Pdk\Facade\Pdk;
@@ -21,7 +20,6 @@ use MyParcelNL\Pdk\Tests\Api\Response\ExamplePostShipmentsResponse;
 use MyParcelNL\Pdk\Tests\Bootstrap\MockPdkOrderRepository;
 use MyParcelNL\Pdk\Tests\Uses\UsesApiMock;
 use MyParcelNL\Pdk\Tests\Uses\UsesMockPdkInstance;
-use RuntimeException;
 use Symfony\Component\HttpFoundation\Response;
 use function DI\autowire;
 use function MyParcelNL\Pdk\Tests\usesShared;
@@ -33,60 +31,82 @@ usesShared(
     new UsesApiMock()
 );
 
-it('exports enitre order with check on customer information', function (array $orders) {
-    /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockApiService $api */
-    $api = Pdk::get(ApiServiceInterface::class);
-    $api->getMock()
-        ->append(new ExamplePostShipmentsResponse());
+it(
+    'exports order without customer information if setting is false',
+    function (bool $share, bool $orderMode, array $orders) {
+        /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockApiService $api */
+        $api = Pdk::get(ApiServiceInterface::class);
+        $api->getMock()
+            ->append($orderMode ? new ExamplePostOrdersResponse() : new ExamplePostShipmentsResponse());
 
-    /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockSettingsRepository $settingsRepository */
-    $settingsRepository = Pdk::get(SettingsRepositoryInterface::class);
-    $settingsRepository->storeSettings(
-        new GeneralSettings([
-            GeneralSettings::ORDER_MODE                 => true,
-            GeneralSettings::SHARE_CUSTOMER_INFORMATION => false,
-        ])
-    );
+        /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockSettingsRepository $settingsRepository */
+        $settingsRepository = Pdk::get(SettingsRepositoryInterface::class);
+        $settingsRepository->storeSettings(
+            new GeneralSettings([
+                GeneralSettings::ORDER_MODE                 => $orderMode,
+                GeneralSettings::SHARE_CUSTOMER_INFORMATION => $share,
+            ])
+        );
 
-    /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockPdkOrderRepository $orderRepository */
-    $orderRepository = Pdk::get(PdkOrderRepositoryInterface::class);
-    $orderRepository->updateMany(new PdkOrderCollection($orders))
-        ->each(function (PdkOrder $order) use ($orderRepository) {
-            $order->setBillingAddress(
-                new ContactDetails([
-                    'email' => 'myparcel@myparcel.nl',
-                    'phone' => '0612345678',
-                ])
-            );
-            $orderRepository->add($order);
-        });
+        /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockPdkOrderRepository $orderRepository */
+        $orderRepository = Pdk::get(PdkOrderRepositoryInterface::class);
+        $orderRepository->updateMany(new PdkOrderCollection($orders));
 
-    $response = Actions::execute(PdkBackendActions::EXPORT_ORDERS, [
-        'orderIds' => Arr::pluck($orders, 'externalIdentifier'),
-    ]);
+        Actions::execute(PdkBackendActions::EXPORT_ORDERS, [
+            'orderIds' => Arr::pluck($orders, 'externalIdentifier'),
+        ]);
 
-    if (! $response) {
-        throw new RuntimeException('Response is empty');
+        $lastRequest = $api->getMock()
+            ->getLastRequest();
+
+        if (! $lastRequest) {
+            throw new Exception('No request was made.');
+        }
+
+        $content = json_decode(
+            $lastRequest->getBody()
+                ->getContents(),
+            true
+        );
+
+        $postedAddress = Arr::get(
+            $content,
+            $orderMode
+                ? 'data.orders.0.invoice_address'
+                : 'data.shipments.0.recipient'
+        );
+
+        expect($postedAddress)->toBeArray();
+
+        if ($share) {
+            expect(Arr::only($postedAddress, ['email', 'phone']))
+                ->each->toBeString();
+        } else {
+            expect(Arr::only($postedAddress, ['email', 'phone']))
+                ->each->toBeNull();
+        }
     }
+)
+    ->with([
+        'share'        => [
+            'share'     => true,
+            'orderMode' => false,
+        ],
+        'do not share' => [
+            'share'     => false,
+            'orderMode' => false,
+        ],
 
-    $content = json_decode($response->getContent(), true);
-
-    $responseOrders    = $content['data']['orders'];
-    $responseShipments = Arr::pluck($responseOrders, 'shipments');
-
-    expect($response)
-        ->toBeInstanceOf(Response::class)
-        ->and(Arr::dot($content))
-        ->and($content['data.orders.0.billingAddress.email'])
-        ->toBeNull()
-        ->and($content['data.orders.0.billingAddress.phone'])
-        ->toBeNull()
-        ->and($responseOrders)
-        ->toHaveLength(count($orders))
-        ->and($responseShipments)->each->toHaveLength(0)
-        ->and($response->getStatusCode())
-        ->toBe(200);
-})->with('pdkOrdersDomestic');
+        'order mode: share'        => [
+            'share'     => true,
+            'orderMode' => true,
+        ],
+        'order mode: do not share' => [
+            'share'     => false,
+            'orderMode' => true,
+        ],
+    ])
+    ->with('pdkOrdersDomestic');
 
 it('exports entire order', function (array $orders) {
     /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockApiService $api */
@@ -109,10 +129,6 @@ it('exports entire order', function (array $orders) {
     $response = Actions::execute(PdkBackendActions::EXPORT_ORDERS, [
         'orderIds' => Arr::pluck($orders, 'externalIdentifier'),
     ]);
-
-    if (! $response) {
-        throw new RuntimeException('Response is empty');
-    }
 
     $content = json_decode($response->getContent(), true);
 
