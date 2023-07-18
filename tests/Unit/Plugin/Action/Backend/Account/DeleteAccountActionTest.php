11433008aeae58b2a9c3ea0f74edb877e290efa7
Edie Lemoine <edie@myparcel.nl>
2023-07-18 11:05:24 +0200

    feat(actions): add delete account action (#124)

---
+++ tests/Unit/Plugin/Action/Backend/Account/DeleteAccountActionTest.php
@@ -0,0 +1,26 @@
+<?php
+/** @noinspection NullPointerExceptionInspection,PhpUnhandledExceptionInspection,StaticClosureCanBeUsedInspection */
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Pdk\Tests\Unit\Plugin\Action\Backend\Account;
+
+use MyParcelNL\Pdk\Account\Model\Account;
+use MyParcelNL\Pdk\App\Api\Backend\PdkBackendActions;
+use MyParcelNL\Pdk\Facade\AccountSettings;
+use MyParcelNL\Pdk\Facade\Actions;
+use MyParcelNL\Pdk\Tests\Uses\UsesMockPdkInstance;
+use function MyParcelNL\Pdk\Tests\usesShared;
+
+usesShared(new UsesMockPdkInstance());
+
+it('deletes account', function () {
+    expect(AccountSettings::getAccount())->toBeInstanceOf(Account::class);
+
+    Actions::execute(PdkBackendActions::DELETE_ACCOUNT);
+
+    $account = AccountSettings::getAccount();
+
+    expect($account)->toBeNull();
+});
+
