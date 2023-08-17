1b497259eb0c8a3bb64900fb7b00e9c65d217941
Edie Lemoine <edie@myparcel.nl>
2023-08-17 09:56:29 +0200

    chore: add phpstorm advanced meta file
    See https://www.jetbrains.com/help/phpstorm/ide-advanced-metadata.html

---
+++ .meta/.phpstorm.meta.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @see https://www.jetbrains.com/help/phpstorm/ide-advanced-metadata.html
+ */
+
+namespace PHPSTORM_META {
+
+    override(\MyParcelNL\Pdk\Facade\Pdk::get(), map(['' => '@']));
+}
