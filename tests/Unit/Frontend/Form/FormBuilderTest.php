7b60ad5ad7a9f53fc677b4b20685fd7e622aceb9
Edie Lemoine <edie@myparcel.nl>
2023-08-23 19:41:52 +0200

    test: update tests

--- tests/Unit/Frontend/Form/FormBuilderTest.php
+++ tests/Unit/Frontend/Form/FormBuilderTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 
 namespace MyParcelNL\Pdk\Frontend\Form;
 
+use MyParcelNL\Pdk\Base\Support\Arr;
+use MyParcelNL\Pdk\Frontend\Form\Builder\FormAfterUpdateBuilder;
 use MyParcelNL\Pdk\Frontend\Form\Element\Contract\ElementBuilderInterface;
 use MyParcelNL\Pdk\Frontend\Form\Element\TextInput;
 
@@ -76,3 +78,35 @@ it('builds interactive elements', function (string $class, string $component) {
             '$component' => $component,
         ]);
 })->with('interactive elements');
+
+it('adds operation builders', function () {
+    $formBuilder = new FormBuilder();
+
+    $elements = [
+        (new TextInput('test1'))->afterUpdate(function (FormAfterUpdateBuilder $builder) {
+            $builder->setValue(2)
+                ->on('test2');
+        }),
+        (new TextInput('test2'))->afterUpdate(function (FormAfterUpdateBuilder $builder) {
+            $builder->setValue(3)
+                ->on('test3');
+        }),
+        new TextInput('test3'),
+    ];
+
+    $formBuilder->add(...$elements);
+
+    $built = $formBuilder->build()
+        ->toArrayWithoutNull();
+
+    $firstTwo = $built;
+    $last     = array_pop($firstTwo);
+
+    expect($built)
+        ->toHaveLength(3)
+        ->and($firstTwo)
+        ->toHaveLength(2)
+        ->and($firstTwo)->each->toHaveKey('$builders')
+        ->and(Arr::pluck($firstTwo, '$builders'))->each->toHaveLength(1)
+        ->and($last)->not->toHaveKey('$builders');
+});
