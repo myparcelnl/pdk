a98efe772f3214c45462c501bb9399a7198df873
joerivanveen <joeri@myparcel.nl>
2023-08-25 14:36:01 +0200

    fix: implement feedback

--- config/pdk-settings.php
+++ config/pdk-settings.php
@@ -105,15 +105,18 @@ return [
      * Callback to generate a settings key.
      */
 
-    'createSettingsKey' => factory(function () {
+    'createSettingsKey'           => factory(function () {
         return static function (string $key) {
             return sprintf('%s%s', PdkFacade::get('settingKeyPrefix'), $key);
         };
     }),
 
+    /**
+     * Callback to generate a barcode note identifier
+     */
     'createBarcodeNoteIdentifier' => factory(function () {
-        return static function (int $shippingId) {
-            return "barcode-$shippingId";
+        return static function (int $shipmentId) {
+            return "barcode-$shipmentId";
         };
     }),
 
