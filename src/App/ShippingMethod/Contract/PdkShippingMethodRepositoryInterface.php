1e40a53b81514c6d5f34e1824fe3230d958d40bf
Edie Lemoine <edie@myparcel.nl>
2023-09-11 20:52:14 +0200

    refactor: remove unused code

--- src/App/ShippingMethod/Contract/PdkShippingMethodRepositoryInterface.php
+++ src/App/ShippingMethod/Contract/PdkShippingMethodRepositoryInterface.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace MyParcelNL\Pdk\App\ShippingMethod\Contract;
 
 use MyParcelNL\Pdk\App\ShippingMethod\Collection\PdkShippingMethodCollection;
-use MyParcelNL\Pdk\App\ShippingMethod\Model\PdkShippingMethod;
 
 interface PdkShippingMethodRepositoryInterface
 {
@@ -13,14 +12,4 @@ interface PdkShippingMethodRepositoryInterface
      * Get all shipping methods.
      */
     public function all(): PdkShippingMethodCollection;
-
-    /**
-     * Get a shipping method.
-     */
-    public function get($input): PdkShippingMethod;
-
-    /**
-     * Get multiple shipping methods.
-     */
-    public function getMany($input): PdkShippingMethodCollection;
 }
--- tests/Bootstrap/MockPdkShippingMethodRepository.php
+++ tests/Bootstrap/MockPdkShippingMethodRepository.php
@@ -5,18 +5,12 @@ declare(strict_types=1);
 namespace MyParcelNL\Pdk\Tests\Bootstrap;
 
 use MyParcelNL\Pdk\App\ShippingMethod\Collection\PdkShippingMethodCollection;
-use MyParcelNL\Pdk\App\ShippingMethod\Model\PdkShippingMethod;
-use MyParcelNL\Pdk\App\ShippingMethod\Repository\AbstractPdkShippingMethodRepository;
+use MyParcelNL\Pdk\App\ShippingMethod\Contract\PdkShippingMethodRepositoryInterface;
 
-class MockPdkShippingMethodRepository extends AbstractPdkShippingMethodRepository
+class MockPdkShippingMethodRepository implements PdkShippingMethodRepositoryInterface
 {
     public function all(): PdkShippingMethodCollection
     {
         return new PdkShippingMethodCollection();
-    }
-
-    public function get($input): PdkShippingMethod
-    {
-        return new PdkShippingMethod();
     }
 }
