4c6ba28da7665712f2b9da1053d3b4e6a5487555
Edie Lemoine <edie@myparcel.nl>
2023-10-16 15:49:40 +0200

    test: increase coverage

--- tests/Unit/App/Api/Service/PdkActionsServiceTest.php
+++ tests/Unit/App/Api/Service/PdkActionsServiceTest.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 
 namespace MyParcelNL\Pdk\App\Api\Service;
 
+use InvalidArgumentException;
+use MyParcelNL\Pdk\Api\Exception\PdkEndpointException;
 use MyParcelNL\Pdk\App\Api\Backend\PdkBackendActions;
 use MyParcelNL\Pdk\App\Api\Contract\PdkActionsServiceInterface;
 use MyParcelNL\Pdk\App\Api\PdkEndpoint;
 use MyParcelNL\Pdk\Facade\Pdk;
 use MyParcelNL\Pdk\Tests\Uses\UsesMockPdkInstance;
+use Symfony\Component\HttpFoundation\Request;
 use function MyParcelNL\Pdk\Tests\usesShared;
 
 usesShared(new UsesMockPdkInstance());
@@ -31,4 +34,34 @@ it('executes actions', function () {
             'action'     => PdkBackendActions::FETCH_ORDERS,
             'parameters' => ['orderIds' => 1],
         ]);
+});
+
+it('throws error if action parameter is missing', function () {
+    /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockPdkActionsService $actions */
+    $actions = Pdk::get(PdkActionsServiceInterface::class);
+
+    expect(function () use ($actions) {
+        $actions->execute(new Request());
+    })->toThrow(InvalidArgumentException::class);
+});
+
+it('throws error if input is not a string or Request', function () {
+    /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockPdkActionsService $actions */
+    $actions = Pdk::get(PdkActionsServiceInterface::class);
+
+    expect(function () use ($actions) {
+        /* @phpstan-ignore-next-line */
+        $actions->execute(1);
+    })->toThrow(InvalidArgumentException::class);
+});
+
+it('throws error if context is invalid', function () {
+    /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockPdkActionsService $actions */
+    $actions = Pdk::get(PdkActionsServiceInterface::class);
+
+    expect(function () use ($actions) {
+        $actions
+            ->setContext('invalid')
+            ->execute(PdkBackendActions::FETCH_ORDERS);
+    })->toThrow(PdkEndpointException::class);
 });
