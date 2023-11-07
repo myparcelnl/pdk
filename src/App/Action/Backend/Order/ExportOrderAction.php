b4b58427e3105f839b256fb4b552d2b5e5743444
Edie Lemoine <edie@myparcel.nl>
2023-11-07 09:44:54 +0100

    Revert "fix(orders): fix exporting shipments occasionally causing visual error"
    This reverts commit 01947ab257ad0e2599d40627242fb4296394f994.

--- src/App/Action/Backend/Order/ExportOrderAction.php
+++ src/App/Action/Backend/Order/ExportOrderAction.php
@@ -136,6 +136,10 @@ class ExportOrderAction extends AbstractOrderAction
 
         $orders->updateShipments($concepts);
 
+        $orders->each(function (PdkOrder $order) {
+            $order->shipments = $order->shipments->take(-1);
+        });
+
         if (! Settings::get(OrderSettings::CONCEPT_SHIPMENTS, OrderSettings::ID)) {
             $this->shipmentRepository->fetchLabelLink($concepts, LabelSettings::FORMAT_A4);
 
