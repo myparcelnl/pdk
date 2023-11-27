10130eb063f33593d7be54527980e5a77fe71099
Edie Lemoine <edie@myparcel.nl>
2023-11-27 13:05:41 +0100

    fix: re-add deprecated class

---
+++ src/Settings/Repository/AbstractSettingsRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Pdk\Settings\Repository;
+
+/**
+ * @deprecated Use AbstractPdkSettingsRepository instead. Will be removed in v3.0.0
+ * @todo       Remove in v3.0.0
+ */
+abstract class AbstractSettingsRepository extends AbstractPdkSettingsRepository { }
