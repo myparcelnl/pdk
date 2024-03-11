e7d66cc9649b7d51fd54d9b5573f0c400d5215ea
markernst97 <markiervc@gmail.com>
2024-03-11 12:56:53 +0100

    fix: disable tracked for nl small packages

--- src/App/Order/Calculator/General/PackageTypeShipmentOptionsCalculator.php
+++ src/App/Order/Calculator/General/PackageTypeShipmentOptionsCalculator.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace MyParcelNL\Pdk\App\Order\Calculator\General;
 
 use MyParcelNL\Pdk\App\Order\Calculator\AbstractPdkOrderOptionCalculator;
+use MyParcelNL\Pdk\Base\Service\CountryCodes;
 use MyParcelNL\Pdk\Shipment\Model\DeliveryOptions;
 use MyParcelNL\Pdk\Shipment\Model\ShipmentOptions;
 use MyParcelNL\Pdk\Types\Service\TriStateService;
@@ -22,6 +23,13 @@ final class PackageTypeShipmentOptionsCalculator extends AbstractPdkOrderOptionC
             return;
         }
 
+        $tracked = TriStateService::DISABLED;
+
+        if (DeliveryOptions::PACKAGE_TYPE_PACKAGE_SMALL_NAME === $deliveryOptions->packageType) {
+            $tracked = $this->order->shippingAddress->cc === CountryCodes::CC_NL ? TriStateService::DISABLED
+                : TriStateService::ENABLED;
+        }
+
         $this->order->deliveryOptions->shipmentOptions->fill([
             ShipmentOptions::AGE_CHECK         => TriStateService::DISABLED,
             ShipmentOptions::DIRECT_RETURN     => TriStateService::DISABLED,
@@ -30,8 +38,7 @@ final class PackageTypeShipmentOptionsCalculator extends AbstractPdkOrderOptionC
             ShipmentOptions::ONLY_RECIPIENT    => TriStateService::DISABLED,
             ShipmentOptions::SAME_DAY_DELIVERY => TriStateService::DISABLED,
             ShipmentOptions::SIGNATURE         => TriStateService::DISABLED,
-            ShipmentOptions::TRACKED           => DeliveryOptions::PACKAGE_TYPE_PACKAGE_SMALL_NAME === $deliveryOptions->packageType
-                ? TriStateService::ENABLED : TriStateService::DISABLED,
+            ShipmentOptions::TRACKED           => $tracked,
         ]);
     }
 }
