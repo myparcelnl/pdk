851a443ea3257c6c4b2601a9d15d6e18b760bb0b
Edie Lemoine <edie@myparcel.nl>
2024-04-04 12:58:16 +0200

    test: improve print orders tests (#264)

--- src/Facade/Actions.php
+++ src/Facade/Actions.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace MyParcelNL\Pdk\Facade;
 
-use MyParcelNL\Pdk\App\Api\Service\PdkActionsService;
+use MyParcelNL\Pdk\App\Api\Contract\PdkActionsServiceInterface;
 use MyParcelNL\Pdk\Base\Facade;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,12 +12,12 @@ use Symfony\Component\HttpFoundation\Response;
 /**
  * @method static Response execute(string|Request $action, array $parameters = [])
  * @method static Response executeAutomatic(string|Request $action, array $parameters = [])
- * @see PdkActionsService
+ * @see \MyParcelNL\Pdk\App\Api\Contract\PdkActionsServiceInterface
  */
 final class Actions extends Facade
 {
     protected static function getFacadeAccessor(): string
     {
-        return PdkActionsService::class;
+        return PdkActionsServiceInterface::class;
     }
 }
