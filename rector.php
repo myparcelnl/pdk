ceefea02a6ca25d74798a66a139626ed74c47e47
Edie Lemoine <edie@myparcel.nl>
2024-05-15 11:46:46 +0200

    build: update rector to ^1.0

--- rector.php
+++ rector.php
@@ -1,4 +1,5 @@
 <?php
+/** @noinspection PhpUnhandledExceptionInspection */
 
 declare(strict_types=1);
 
@@ -9,51 +10,33 @@ use Rector\CodingStyle\Rector\Encapsed\EncapsedStringsToSprintfRector;
 use Rector\Config\RectorConfig;
 use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
 
-// TODO: Configure setlists
-return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->cacheClass(FileCacheStorage::class);
-    $rectorConfig->cacheDirectory(__DIR__ . '/.cache/rector');
-
-    $rectorConfig->paths([
+return RectorConfig::configure()
+    ->withCache(__DIR__ . '/.cache/rector', FileCacheStorage::class)
+    ->withPaths([
         __DIR__ . '/config',
         __DIR__ . '/private',
         __DIR__ . '/src',
         __DIR__ . '/tests',
-    ]);
-
-    $rectorConfig->sets([
-        // LevelSetList::UP_TO_PHP_71,
-        // SetList::CODE_QUALITY,
-        // SetList::CODING_STYLE,
-        // SetList::DEAD_CODE,
-        // SetList::EARLY_RETURN,
-        // SetList::INSTANCEOF,
-        // SetList::PRIVATIZATION,
-        // SetList::TYPE_DECLARATION,
-    ]);
-
-    $rectorConfig->rules([
+    ])
+    // TODO: Configure setlists
+    ->withSets([])
+    ->withRules([
         DeclareStrictTypesRector::class,
-    ]);
-
-    $rectorConfig->rule(DeclareStrictTypesRector::class);
-
+    ])
     /**
      * Global overrides.
      */
-    $rectorConfig->skip([
+    ->withSkip([
         /**
          * Replaces string interpolation with sprintf calls. This is not always desirable. From SetList::CODING_STYLE.
          */
         EncapsedStringsToSprintfRector::class,
-    ]);
-
+    ])
     /**
      * Overrides for tests.
      */
-    $rectorConfig
-        ->skip([
-            StaticArrowFunctionRector::class => [__DIR__ . '/tests'],
-            StaticClosureRector::class       => [__DIR__ . '/tests'],
-        ]);
-};
+    ->withSkip([
+        StaticArrowFunctionRector::class => [__DIR__ . '/tests'],
+        StaticClosureRector::class       => [__DIR__ . '/tests'],
+    ]);
+
