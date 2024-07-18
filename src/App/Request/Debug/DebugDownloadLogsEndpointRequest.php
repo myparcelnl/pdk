9907455df5f16054884c249dd5327e93409e5a3b
Edie Lemoine <edie@myparcel.nl>
2024-07-18 11:37:24 +0200

    feat(debug): add action to download logs

---
+++ src/App/Request/Debug/DebugDownloadLogsEndpointRequest.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Pdk\App\Request\Debug;
+
+use MyParcelNL\Pdk\App\Request\AbstractEndpointRequest;
+
+class DebugDownloadLogsEndpointRequest extends AbstractEndpointRequest { }
