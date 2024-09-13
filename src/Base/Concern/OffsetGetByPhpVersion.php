c289e2904f934d0ad78c5249fe669371d0a89f62
Edie Lemoine <edie@myparcel.nl>
2024-09-13 11:47:56 +0200

    chore: change php version in comment

--- src/Base/Concern/OffsetGetByPhpVersion.php
+++ src/Base/Concern/OffsetGetByPhpVersion.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace MyParcelNL\Pdk\Base\Concern;
 
 /*
- * Ugly fix for the following error in php 7:
+ * Ugly fix for the following error in php 8:
  *   Return type of MyParcelNL\Pdk\Base\Model\Model::offsetGet($offset) should either be compatible with
  *   ArrayAccess::offsetGet(mixed $offset): mixed, or the #[\ReturnTypeWillChange] attribute should be used to
  *   temporarily suppress the notice.
