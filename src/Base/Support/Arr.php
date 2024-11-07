e84a3947a98b60abcf915ca03aae42653c18662e
Edie Lemoine <edie@myparcel.nl>
2024-11-07 17:00:46 +0100

    feat!: replace arr with illuminate arr

--- src/Base/Support/Arr.php
+++ src/Base/Support/Arr.php
@@ -4,41 +4,4 @@ declare(strict_types=1);
 
 namespace MyParcelNL\Pdk\Base\Support;
 
-use MyParcelNL\Sdk\src\Support\Arr as SdkArr;
-
-class Arr extends SdkArr
-{
-    /**
-     * @var \MyParcelNL\Pdk\Base\Support\Helpers
-     */
-    private static $helpers;
-
-    /**
-     * @param  array $dotted
-     *
-     * @return array
-     */
-    public static function undot(array $dotted): array
-    {
-        $array = [];
-
-        foreach ($dotted as $key => $value) {
-            self::getHelpers()
-                ->data_set($array, $key, $value);
-        }
-
-        return $array;
-    }
-
-    /**
-     * @return \MyParcelNL\Pdk\Base\Support\Helpers
-     */
-    protected static function getHelpers(): Helpers
-    {
-        if (! self::$helpers) {
-            self::$helpers = new Helpers();
-        }
-
-        return self::$helpers;
-    }
-}
+class Arr extends \Illuminate\Support\Arr { }
