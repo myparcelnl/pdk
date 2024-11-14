5ef784eb5b5c32e92e3e2e1e6e584fa40dccba7e
Edie Lemoine <edie@myparcel.nl>
2024-11-14 15:57:28 +0100

    test: add test for changing string case

--- tests/Unit/Base/Support/StrTest.php
+++ tests/Unit/Base/Support/StrTest.php
@@ -17,3 +17,32 @@ it('limits string length', function (string $string, int $length, string $end, s
     ['short enough', 40, '_-.', 'short enough'],
     ['broccoli', 5, '~', 'broc~'],
 ]);
+
+it('changes string case', function (string $string, ?int $flags, string $result) {
+    expect(Str::changeCase($string, $flags))->toBe($result);
+})->with([
+    ['my string', null, 'myString'],
+    ['my string', Str::CASE_KEBAB, 'my-string'],
+    ['my string', Str::CASE_SNAKE, 'my_string'],
+    ['my string', Str::CASE_STUDLY, 'MyString'],
+
+    ['myString', null, 'myString'],
+    ['myString', Str::CASE_SNAKE, 'my_string'],
+    ['myString', Str::CASE_KEBAB, 'my-string'],
+    ['myString', Str::CASE_STUDLY, 'MyString'],
+
+    ['my-string', null, 'myString'],
+    // Kebab can't be converted to snake
+    ['my-string', Str::CASE_KEBAB, 'my-string'],
+    ['my-string', Str::CASE_STUDLY, 'MyString'],
+
+    ['my_string', null, 'myString'],
+    ['my_string', Str::CASE_SNAKE, 'my_string'],
+    // Snake can't be converted to kebab
+    ['my_string', Str::CASE_STUDLY, 'MyString'],
+
+    ['MyString', null, 'myString'],
+    ['MyString', Str::CASE_SNAKE, 'my_string'],
+    ['MyString', Str::CASE_KEBAB, 'my-string'],
+    ['MyString', Str::CASE_STUDLY, 'MyString'],
+]);
