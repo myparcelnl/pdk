7115dd848c782a233e33a9b17d3cbf5ada302a81
Florian de Vries <florian.devries@myparcel.nl>
2024-11-21 12:13:38 +0100

    wip: renamed files

---
+++ src/App/Order/Calculator/UPS/UPSAgeCheckCalculator.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Pdk\App\Order\Calculator\UPS;
+
+use MyParcelNL\Pdk\App\Order\Calculator\AbstractPdkOrderOptionCalculator;
+use MyParcelNL\Pdk\Types\Service\TriStateService;
+
+/**
+ * When age check is enabled, signature and only recipient are required.
+ */
+final class UPSAgeCheckCalculator extends AbstractPdkOrderOptionCalculator
+{
+    public function calculate(): void
+    {
+        $shipmentOptions = $this->order->deliveryOptions->shipmentOptions;
+
+        if (TriStateService::ENABLED !== $shipmentOptions->ageCheck) {
+            return;
+        }
+
+        $shipmentOptions->signature     = TriStateService::ENABLED;
+        $shipmentOptions->onlyRecipient = TriStateService::ENABLED;
+    }
+}
