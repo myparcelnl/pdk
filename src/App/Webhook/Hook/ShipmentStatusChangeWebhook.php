5f869e279caecb93c024bcdc19a92c747e10e4f2
Joeri van Veen <joeri@myparcel.nl>
2024-12-11 21:30:31 +0100

    refactor: prevent unnecessary early return

--- src/App/Webhook/Hook/ShipmentStatusChangeWebhook.php
+++ src/App/Webhook/Hook/ShipmentStatusChangeWebhook.php
@@ -25,15 +25,13 @@ final class ShipmentStatusChangeWebhook extends AbstractHook
         // translate order_id (which is api identifier / uuid) to local order id for db
         $order = Pdk::get(PdkOrderRepositoryInterface::class)->getByApiIdentifier($content['order_id']);
 
-        if (! $order) {
-            return;
+        if ($order) {
+            Actions::execute(PdkBackendActions::UPDATE_SHIPMENTS, [
+                'orderIds'                      => [$order->getExternalIdentifier()],
+                'shipmentIds'                   => [$content['shipment_id']],
+                'linkFirstShipmentToFirstOrder' => true,
+            ]);
         }
-
-        Actions::execute(PdkBackendActions::UPDATE_SHIPMENTS, [
-            'orderIds'                      => [$order->getExternalIdentifier()],
-            'shipmentIds'                   => [$content['shipment_id']],
-            'linkFirstShipmentToFirstOrder' => true,
-        ]);
     }
 
 
