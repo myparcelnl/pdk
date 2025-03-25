f7b2d77d48ce7dc441bc427bcdb2a5a666eb9d11
jochemgravendeel <jochem.gravendeel@myparcel.nl>
2025-03-25 16:21:34 +0100

    fix: update code from feedback

---
+++ src/Api/PdkAddressActions.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Pdk\Api;
+
+/**
+ * Constants for address-related actions in the PDK
+ */
+class PdkAddressActions
+{
+    public const PROXY_ADDRESSES_LIST = 'proxy_addresses_list';
+    public const PROXY_ADDRESSES_VALIDATE = 'proxy_addresses_validate';
+} 
