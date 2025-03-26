f109e987732b37ccfe7209ca360bd7548e917daa
jochemgravendeel <jochem.gravendeel@myparcel.nl>
2025-03-26 16:34:08 +0100

    fix:

---
+++ src/App/Action/Addresses/AddressesValidateAction.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Pdk\App\Action\Addresses;
+
+use MyParcelNL\Pdk\App\Action\Contract\ActionInterface;
+use MyParcelNL\Pdk\Api\Service\AddressesApiService;
+use MyParcelNL\Pdk\Api\Response\JsonResponse;
+use MyParcelNL\Pdk\Api\Request\ProxyRequest;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AddressesValidateAction implements ActionInterface
+{
+    /**
+     * @var \MyParcelNL\Pdk\Api\Service\AddressesApiService
+     */
+    private $apiService;
+
+    /**
+     * @param \MyParcelNL\Pdk\Api\Service\AddressesApiService $apiService
+     */
+    public function __construct(AddressesApiService $apiService)
+    {
+        $this->apiService = $apiService;
+    }
+
+    /**
+     * @param  \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function handle(Request $request): Response
+    {
+        $query = $request->query->all();
+
+        // Ensure required parameters are present with correct format
+        $queryParams = [
+            'countryCode' => $query['cc'] ?? 'NL',
+            'postalCode' => $query['postalCode'] ?? null,
+            'houseNumber' => $query['houseNumber'] ?? null,
+            'query' => $query['query'] ?? null,
+        ];
+
+        // Filter out null values
+        $queryParams = array_filter($queryParams, function ($value) {
+            return $value !== null;
+        });
+
+        $proxyRequest = new ProxyRequest(
+            'GET',
+            '/validate',
+            null,
+            $queryParams
+        );
+
+        $response = $this->apiService->doRequest($proxyRequest);
+        $responseData = json_decode($response->getBody() ?? '{"data":[],"errors":[]}', true);
+
+        return new JsonResponse($responseData);
+    }
+} 
