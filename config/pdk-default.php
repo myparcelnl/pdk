23b7791424b489289ce93f788299e16bb72e8376
jochemgravendeel <jochem.gravendeel@myparcel.nl>
2025-03-31 09:11:17 +0200

    fix: implement cors using fruitcake/php-cors

--- config/pdk-default.php
+++ config/pdk-default.php
@@ -42,7 +42,7 @@ return [
     /**
      * Security settings for the proxy
      */
-    'allowedProxyHosts'      => env('PDK_ALLOWED_PROXY_HOSTS', ['myparcel.local', 'pdk.myparcel.nl']),
+    'allowedProxyHosts'      => env('PDK_ALLOWED_PROXY_HOSTS', ['self']),
     'allowedProxyOrigins'    => env('PDK_ALLOWED_PROXY_ORIGINS', ['self']),
 
     /**
