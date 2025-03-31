b86e407f41ab2ccd19c0fa7807e962dbcb07d463
jochemgravendeel <jochem.gravendeel@myparcel.nl>
2025-03-31 09:11:17 +0200

    fix: implement cors using fruitcake/php-cors

--- tests/MyParcelNL/Pdk/Tests/Unit/Api/Controller/AddressesProxyControllerTest.php
+++ tests/MyParcelNL/Pdk/Tests/Unit/Api/Controller/AddressesProxyControllerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace MyParcelNL\Pdk\Tests\Unit\Api\Controller;
 
+use Exception;
 use MyParcelNL\Pdk\Api\Controller\AddressesProxyController;
 use MyParcelNL\Pdk\Api\Handler\CorsHandler;
 use MyParcelNL\Pdk\Api\Request\ProxyRequest;
@@ -21,26 +22,78 @@ class AddressesProxyControllerTest extends TestCase
     private $addressesApiService;
 
     /**
-     * @var CorsHandler|MockObject
-     */
-    private $corsHandler;
-
-    /**
      * @var AddressesProxyController
      */
     private $controller;
 
-    protected function setUp(): void
+    /**
+     * @var CorsHandler|MockObject
+     */
+    private $corsHandler;
+
+    public function testProxyWithAddressesServiceError(): void
     {
-        parent::setUp();
+        $request = new Request();
+        $request->headers->set('Origin', 'https://example.com');
 
-        $this->addressesApiService = $this->createMock(AddressesApiService::class);
-        $this->corsHandler = $this->createMock(CorsHandler::class);
+        $proxyRequest = new ProxyRequest('GET', '/addresses', null, [], []);
 
-        $this->controller = new AddressesProxyController(
-            $this->addressesApiService,
-            $this->corsHandler
-        );
+        $this->corsHandler
+            ->expects($this->once())
+            ->method('handlePreflight')
+            ->with($request)
+            ->willReturn(null);
+
+        $this->addressesApiService
+            ->expects($this->once())
+            ->method('doRequest')
+            ->with($proxyRequest)
+            ->willThrowException(new Exception('Invalid postal code format'));
+
+        $this->corsHandler
+            ->expects($this->once())
+            ->method('addCorsHeaders')
+            ->with($request, $this->isInstanceOf(Response::class));
+
+        $response = $this->controller->proxy($request, '/addresses');
+
+        $this->assertEquals(Response::HTTP_INTERNAL_SERVER_ERROR, $response->getStatusCode());
+        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
+
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('error', $responseData);
+        $this->assertEquals('Invalid postal code format', $responseData['error']);
+    }
+
+    public function testProxyWithCorrectCorsHeaders(): void
+    {
+        $request = new Request();
+        $request->headers->set('Origin', 'https://example.com');
+
+        $proxyRequest = new ProxyRequest('GET', '/addresses', null, [], []);
+        $apiResponse  = new Response('{"data": []}', Response::HTTP_OK);
+
+        $this->corsHandler
+            ->expects($this->once())
+            ->method('handlePreflight')
+            ->with($request)
+            ->willReturn(null);
+
+        $this->addressesApiService
+            ->expects($this->once())
+            ->method('doRequest')
+            ->with($proxyRequest)
+            ->willReturn($apiResponse);
+
+        $this->corsHandler
+            ->expects($this->once())
+            ->method('addCorsHeaders')
+            ->with($request, $this->isInstanceOf(Response::class));
+
+        $response = $this->controller->proxy($request, '/addresses');
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
     }
 
     public function testProxyWithCorsPreflight(): void
@@ -50,7 +103,7 @@ class AddressesProxyControllerTest extends TestCase
         $request->headers->set('Origin', 'https://example.com');
 
         $expectedResponse = new Response('', Response::HTTP_NO_CONTENT);
-        
+
         $this->corsHandler
             ->expects($this->once())
             ->method('handlePreflight')
@@ -60,6 +113,61 @@ class AddressesProxyControllerTest extends TestCase
         $response = $this->controller->proxy($request, '/test');
 
         $this->assertSame($expectedResponse, $response);
+    }
+
+    public function testProxyWithSuccessfulAddressesResponse(): void
+    {
+        $request = new Request();
+        $request->headers->set('Origin', 'https://example.com');
+
+        $proxyRequest = new ProxyRequest('GET', '/addresses', null, [], []);
+        $apiResponse  = new Response(
+            json_encode([
+                'data' => [
+                    [
+                        'id'          => 1,
+                        'street'      => 'Test Street',
+                        'number'      => '123',
+                        'postal_code' => '1234 AB',
+                        'city'        => 'Test City',
+                        'country'     => 'NL',
+                    ],
+                ],
+            ]),
+            Response::HTTP_OK
+        );
+
+        $this->corsHandler
+            ->expects($this->once())
+            ->method('handlePreflight')
+            ->with($request)
+            ->willReturn(null);
+
+        $this->addressesApiService
+            ->expects($this->once())
+            ->method('doRequest')
+            ->with($proxyRequest)
+            ->willReturn($apiResponse);
+
+        $this->corsHandler
+            ->expects($this->once())
+            ->method('addCorsHeaders')
+            ->with($request, $this->isInstanceOf(Response::class));
+
+        $response = $this->controller->proxy($request, '/addresses');
+
+        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
+        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
+
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('data', $responseData);
+        $this->assertIsArray($responseData['data']);
+        $this->assertCount(1, $responseData['data']);
+        $this->assertArrayHasKey('street', $responseData['data'][0]);
+        $this->assertArrayHasKey('number', $responseData['data'][0]);
+        $this->assertArrayHasKey('postal_code', $responseData['data'][0]);
+        $this->assertArrayHasKey('city', $responseData['data'][0]);
+        $this->assertArrayHasKey('country', $responseData['data'][0]);
     }
 
     public function testProxyWithUnauthorizedOrigin(): void
@@ -79,66 +187,16 @@ class AddressesProxyControllerTest extends TestCase
         $this->assertEquals('Unauthorized origin', $response->getContent());
     }
 
-    public function testProxyWithSuccessfulRequest(): void
+    protected function setUp(): void
     {
-        $request = new Request();
-        $request->headers->set('Origin', 'https://example.com');
+        parent::setUp();
 
-        $proxyRequest = new ProxyRequest('GET', '/test', null, [], []);
-        $apiResponse = new Response('{"data": "test"}', Response::HTTP_OK);
-        
-        $this->corsHandler
-            ->expects($this->once())
-            ->method('handlePreflight')
-            ->with($request)
-            ->willReturn(null);
+        $this->addressesApiService = $this->createMock(AddressesApiService::class);
+        $this->corsHandler         = $this->createMock(CorsHandler::class);
 
-        $this->addressesApiService
-            ->expects($this->once())
-            ->method('doRequest')
-            ->with($proxyRequest)
-            ->willReturn($apiResponse);
-
-        $this->corsHandler
-            ->expects($this->once())
-            ->method('addCorsHeaders')
-            ->with($request, $this->isInstanceOf(Response::class));
-
-        $response = $this->controller->proxy($request, '/test');
-
-        $this->assertEquals(Response::HTTP_OK, $response->getStatusCode());
-        $this->assertEquals('{"data": "test"}', $response->getContent());
-        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
+        $this->controller = new AddressesProxyController(
+            $this->addressesApiService,
+            $this->corsHandler
+        );
     }
-
-    public function testProxyWithError(): void
-    {
-        $request = new Request();
-        $request->headers->set('Origin', 'https://example.com');
-
-        $proxyRequest = new ProxyRequest('GET', '/test', null, [], []);
-        
-        $this->corsHandler
-            ->expects($this->once())
-            ->method('handlePreflight')
-            ->with($request)
-            ->willReturn(null);
-
-        $this->addressesApiService
-            ->expects($this->once())
-            ->method('doRequest')
-            ->with($proxyRequest)
-            ->willThrowException(new \Exception('API Error'));
-
-        $this->corsHandler
-            ->expects($this->once())
-            ->method('addCorsHeaders')
-            ->with($request, $this->isInstanceOf(Response::class));
-
-        $response = $this->controller->proxy($request, '/test');
-
-        $this->assertEquals(Response::HTTP_INTERNAL_SERVER_ERROR, $response->getStatusCode());
-        $this->assertEquals('{"error":"API Error"}', $response->getContent());
-        $this->assertEquals('application/json', $response->headers->get('Content-Type'));
-    }
-} 
+}
