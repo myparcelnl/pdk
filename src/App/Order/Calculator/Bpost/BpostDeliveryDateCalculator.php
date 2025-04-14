3021457743008267d388cc750433c8fa8beac933
jochemgravendeel <jochem.gravendeel@myparcel.nl>
2025-04-14 13:42:26 +0200

    fix: write docblock

--- src/App/Order/Calculator/Bpost/BpostDeliveryDateCalculator.php
+++ src/App/Order/Calculator/Bpost/BpostDeliveryDateCalculator.php
@@ -8,6 +8,9 @@ use MyParcelNL\Pdk\App\Order\Calculator\AbstractPdkOrderOptionCalculator;
 
 final class BpostDeliveryDateCalculator extends AbstractPdkOrderOptionCalculator
 {
+    /**
+     * Sets the delivery date to null as Bpost carrier does not accept delivery dates.
+     */
     public function calculate(): void
     {
         $this->order->deliveryOptions->date = null;
