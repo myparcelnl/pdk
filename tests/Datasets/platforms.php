7a36791b0ac8380dfe609663c9fa340e1397bc42
Joeri van Veen <joeri@myparcel.nl>
2025-09-16 21:35:40 +0200

    fix: implement feedback

--- tests/Datasets/platforms.php
+++ tests/Datasets/platforms.php
@@ -6,6 +6,6 @@ declare(strict_types=1);
 use MyParcelNL\Pdk\Account\Platform;
 
 dataset('platforms', function () {
-    yield Platform::SENDMYPARCEL_NAME;
     yield Platform::MYPARCEL_NAME;
+    yield Platform::SENDMYPARCEL_NAME;
 });
