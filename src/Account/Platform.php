2afdcb8781619ed2027e3768890e873b62680742
Freek van Rijt <freek.vanrijt@myparcel.nl>
2025-09-30 15:12:31 +0200

    refactor!: remove all flespakket configs and snapshots

--- src/Account/Platform.php
+++ src/Account/Platform.php
@@ -16,13 +16,11 @@ class Platform
     public const LEGACY_SENDMYPARCEL_NAME = 'sendmyparcel';
 
     public const PLATFORMS_TO_LEGACY_MAP = [
-        self::FLESPAKKET_NAME => self::FLESPAKKET_NAME,
         self::MYPARCEL_NAME   => self::LEGACY_MYPARCEL_NAME,
         self::SENDMYPARCEL_NAME => self::LEGACY_SENDMYPARCEL_NAME
     ];
 
     public const PLATFORM_ID_TO_NAME_MAP = [
-        self::FLESPAKKET_ID     => self::FLESPAKKET_NAME,
         self::MYPARCEL_ID       => self::MYPARCEL_NAME,
         self::SENDMYPARCEL_ID   => self::SENDMYPARCEL_NAME,
     ];
