06ef711d5864f8d94c0d50ce8ea0f5dcb60755f5
jochemgravendeel <jochem.gravendeel@myparcel.nl>
2025-10-01 14:20:46 +0200

    fix: update delete account action

--- src/App/Action/Backend/Account/DeleteAccountAction.php
+++ src/App/Action/Backend/Account/DeleteAccountAction.php
@@ -6,6 +6,8 @@ namespace MyParcelNL\Pdk\App\Action\Backend\Account;
 
 use MyParcelNL\Pdk\App\Api\Backend\PdkBackendActions;
 use MyParcelNL\Pdk\Facade\Actions;
+use MyParcelNL\Pdk\Settings\Contract\PdkSettingsRepositoryInterface;
+use MyParcelNL\Pdk\Settings\Model\AccountSettings;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -18,7 +20,13 @@ final class DeleteAccountAction extends UpdateAccountAction
      */
     public function handle(Request $request): Response
     {
-        $this->updateAccountSettings([]);
+        // Explicitly delete all account settings, including environment
+        $this->pdkSettingsRepository->storeSettings(new AccountSettings([
+            'id' => AccountSettings::ID,
+            'apiKey' => null,
+            'apiKeyValid' => false,
+            'environment' => null
+        ]));
 
         return Actions::execute(PdkBackendActions::UPDATE_ACCOUNT);
     }
