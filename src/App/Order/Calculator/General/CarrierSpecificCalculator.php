8cccf492a8930f81fbadf42c5b5325500ee18f20
Freek van Rijt <freek.vanrijt@myparcel.nl>
2025-10-22 12:02:06 +0200

    fix: make carrier specific calculator work with both legacy and new carrier names

--- src/App/Order/Calculator/General/CarrierSpecificCalculator.php
+++ src/App/Order/Calculator/General/CarrierSpecificCalculator.php
@@ -16,6 +16,8 @@ use MyParcelNL\Pdk\App\Order\Calculator\UPSStandard\UPSStandardCalculator;
 use MyParcelNL\Pdk\App\Order\Calculator\UPSExpressSaver\UPSExpressSaverCalculator;
 use MyParcelNL\Pdk\App\Order\Contract\PdkOrderOptionCalculatorInterface;
 use MyParcelNL\Pdk\Carrier\Model\Carrier;
+use MyParcelNL\Pdk\Facade\Pdk;
+use MyParcelNL\Pdk\Proposition\Service\PropositionService;
 
 final class CarrierSpecificCalculator extends AbstractPdkOrderOptionCalculator
 {
@@ -24,20 +26,20 @@ final class CarrierSpecificCalculator extends AbstractPdkOrderOptionCalculator
      * @var array<string, class-string<PdkOrderOptionCalculatorInterface>>
      */
     private const CARRIER_CALCULATOR_MAP = [
-        Carrier::CARRIER_POSTNL_LEGACY_NAME             => PostNLCalculator::class,
-        Carrier::CARRIER_DHL_FOR_YOU_LEGACY_NAME        => DhlForYouCalculator::class,
-        Carrier::CARRIER_DHL_EUROPLUS_LEGACY_NAME       => DhlEuroplusCalculator::class,
-        Carrier::CARRIER_DHL_PARCEL_CONNECT_LEGACY_NAME => DhlParcelConnectCalculator::class,
-        Carrier::CARRIER_DPD_LEGACY_NAME                => DpdCalculator::class,
-        Carrier::CARRIER_UPS_STANDARD_LEGACY_NAME       => UPSStandardCalculator::class,
-        Carrier::CARRIER_UPS_EXPRESS_SAVER_LEGACY_NAME  => UPSExpressSaverCalculator::class,
-        Carrier::CARRIER_BPOST_LEGACY_NAME              => BpostCalculator::class,
-        Carrier::CARRIER_GLS_LEGACY_NAME                => GlsCalculator::class,
+        Carrier::CARRIER_POSTNL_NAME             => PostNLCalculator::class,
+        Carrier::CARRIER_DHL_FOR_YOU_NAME        => DhlForYouCalculator::class,
+        Carrier::CARRIER_DHL_EUROPLUS_NAME       => DhlEuroplusCalculator::class,
+        Carrier::CARRIER_DHL_PARCEL_CONNECT_NAME => DhlParcelConnectCalculator::class,
+        Carrier::CARRIER_DPD_NAME                => DpdCalculator::class,
+        Carrier::CARRIER_UPS_STANDARD_NAME       => UPSStandardCalculator::class,
+        Carrier::CARRIER_UPS_EXPRESS_SAVER_NAME  => UPSExpressSaverCalculator::class,
+        Carrier::CARRIER_BPOST_NAME              => BpostCalculator::class,
+        Carrier::CARRIER_GLS_NAME                => GlsCalculator::class,
     ];
 
     public function calculate(): void
     {
-        $carrierName = $this->order->deliveryOptions->carrier->name;
+        $carrierName = Pdk::get(PropositionService::class)->mapLegacyToNewCarrierName($this->order->deliveryOptions->carrier->name);
         $calculator  = self::CARRIER_CALCULATOR_MAP[$carrierName] ?? null;
 
         if (! $calculator) {
