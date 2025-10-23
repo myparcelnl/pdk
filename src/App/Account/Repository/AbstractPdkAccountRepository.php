cfd3069b9357052d0579771a7efc4fcf801db5c1
Joeri van Veen <joeri@myparcel.nl>
2025-10-23 16:54:23 +0200

    fix: allow filling in api key
    INT-1175

--- src/App/Account/Repository/AbstractPdkAccountRepository.php
+++ src/App/Account/Repository/AbstractPdkAccountRepository.php
@@ -8,11 +8,8 @@ use MyParcelNL\Pdk\Account\Model\Account;
 use MyParcelNL\Pdk\Account\Repository\AccountRepository;
 use MyParcelNL\Pdk\App\Account\Contract\PdkAccountRepositoryInterface;
 use MyParcelNL\Pdk\Base\Repository\Repository;
-use MyParcelNL\Pdk\Facade\Settings;
 use MyParcelNL\Pdk\Settings\Contract\PdkSettingsRepositoryInterface;
-use MyParcelNL\Pdk\Settings\Model\AccountSettings;
 use MyParcelNL\Pdk\Storage\Contract\StorageInterface;
-use Throwable;
 
 abstract class AbstractPdkAccountRepository extends Repository implements PdkAccountRepositoryInterface
 {
@@ -60,53 +57,11 @@ abstract class AbstractPdkAccountRepository extends Repository implements PdkAcc
         }
 
         return $this->retrieve('account', function () {
-            $apiKey = Settings::get(AccountSettings::API_KEY, AccountSettings::ID);
-
-            if ($this->isInvalidApiKey($apiKey)) {
+            if (! $this->settingsRepository->all()->account->apiKeyValid) {
                 return null;
             }
 
-            try {
-                $account = $this->accountRepository->getAccount();
-
-                $this->setApiKeyValidity(true);
-
-                return $account;
-            } catch (Throwable $e) {
-                $this->setApiKeyValidity(false);
-                throw $e;
-            }
+            return $this->accountRepository->getAccount();
         }, $force);
-    }
-
-    /**
-     * @param  null|string $apiKey
-     *
-     * @return bool
-     */
-    protected function isInvalidApiKey(?string $apiKey): bool
-    {
-        if (! $apiKey) {
-            return true;
-        }
-
-        $accountSettings = $this->settingsRepository->all()->account;
-
-        return $accountSettings->apiKey !== $apiKey;
-    }
-
-    /**
-     * @param  bool $apiKeyIsValid
-     *
-     * @return void
-     */
-    protected function setApiKeyValidity(bool $apiKeyIsValid): void
-    {
-        $accountSettings = $this->settingsRepository->all()->account
-            ->fill([
-                AccountSettings::API_KEY_VALID => $apiKeyIsValid,
-            ]);
-
-        $this->settingsRepository->storeSettings($accountSettings);
     }
 }
--- src/App/Action/Backend/Account/UpdateAccountAction.php
+++ src/App/Action/Backend/Account/UpdateAccountAction.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace MyParcelNL\Pdk\App\Action\Backend\Account;
 
 use MyParcelNL\Pdk\Account\Model\Account;
+use MyParcelNL\Pdk\Account\Repository\AccountRepository;
 use MyParcelNL\Pdk\Account\Repository\ShopCarrierConfigurationRepository;
 use MyParcelNL\Pdk\Account\Repository\ShopCarrierOptionsRepository;
 use MyParcelNL\Pdk\App\Account\Contract\PdkAccountRepositoryInterface;
@@ -15,12 +16,16 @@ use MyParcelNL\Pdk\Context\Context;
 use MyParcelNL\Pdk\Facade\Actions;
 use MyParcelNL\Pdk\Settings\Contract\PdkSettingsRepositoryInterface;
 use MyParcelNL\Pdk\Settings\Model\AccountSettings;
-use RuntimeException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class UpdateAccountAction implements ActionInterface
 {
+    /**
+     * @var \MyParcelNL\Pdk\Account\Repository\AccountRepository
+     */
+    protected $accountRepository;
+
     /**
      * @var \MyParcelNL\Pdk\Account\Repository\ShopCarrierConfigurationRepository
      */
@@ -51,12 +56,14 @@ class UpdateAccountAction implements ActionInterface
         ShopCarrierConfigurationRepository $carrierConfigurationRepository,
         ShopCarrierOptionsRepository       $carrierOptionsRepository,
         PdkSettingsRepositoryInterface     $pdkSettingsRepository,
-        PdkAccountRepositoryInterface      $pdkAccountRepository
+        PdkAccountRepositoryInterface      $pdkAccountRepository,
+        AccountRepository                  $accountRepository
     ) {
         $this->carrierConfigurationRepository = $carrierConfigurationRepository;
         $this->carrierOptionsRepository       = $carrierOptionsRepository;
         $this->pdkSettingsRepository          = $pdkSettingsRepository;
         $this->pdkAccountRepository           = $pdkAccountRepository;
+        $this->accountRepository              = $accountRepository;
     }
 
     /**
@@ -94,6 +101,26 @@ class UpdateAccountAction implements ActionInterface
         $shop->carriers              = $this->carrierOptionsRepository->getCarrierOptions($shop->id);
     }
 
+    /**
+     * Stores the validity of the api key in the account, for use in
+     * src/App/Account/Repository/AbstractPdkAccountRepository.php
+     * When the user rotates the key in the backoffice, the plugin will start receiving 401 responses from the API,
+     * however for the purpose of the PDK the api key is still valid. Upon receiving the errors the user will no doubt
+     * update their api key in the settings at which point the validity is reassessed here (updateAndSaveAccount).
+     *
+     * @param  bool $apiKeyIsValid
+     *
+     * @return void
+     */
+    protected function setApiKeyValidity(bool $apiKeyIsValid): void
+    {
+        $accountSettings = $this->pdkSettingsRepository->all()->account
+            ->fill([
+                AccountSettings::API_KEY_VALID => $apiKeyIsValid,
+            ]);
+
+        $this->pdkSettingsRepository->storeSettings($accountSettings);
+    }
 
     /**
      * @param  array $settings
@@ -104,10 +131,10 @@ class UpdateAccountAction implements ActionInterface
     {
         // Get existing account settings to preserve them
         $existingSettings = $this->pdkSettingsRepository->all()->account;
-        
+
         // Always merge with existing settings
-        $existingData = $existingSettings->toArray();
-        $mergedData = array_merge($existingData, $settings);
+        $existingData    = $existingSettings->toArray();
+        $mergedData      = array_merge($existingData, $settings);
         $accountSettings = new AccountSettings($mergedData);
 
         $this->pdkSettingsRepository->storeSettings($accountSettings);
@@ -119,21 +146,28 @@ class UpdateAccountAction implements ActionInterface
      * @param  \MyParcelNL\Pdk\Settings\Model\AccountSettings $accountSettings
      *
      * @return void
+     * @throws \Throwable
      */
     protected function updateAndSaveAccount(AccountSettings $accountSettings): void
     {
-        $foundAccount = $accountSettings->apiKey
-            ? $this->pdkAccountRepository->getAccount(true)
-            : null;
-
-        if ($foundAccount) {
-            $this->fillAccount($foundAccount);
+        if (! $accountSettings->apiKey) {
+            $this->setApiKeyValidity(false);
+            $this->pdkAccountRepository->store(null);
+            return;
         }
 
-        $this->pdkAccountRepository->store($foundAccount);
-
-        if ($foundAccount) {
-            Actions::execute(PdkBackendActions::UPDATE_SUBSCRIPTION_FEATURES);
+        // this try is for the case when the UI is no longer supplying an empty api key when you click "remove api key"
+        try {
+            $account = $this->accountRepository->getAccount();
+        } catch (\Throwable $e) {
+            $this->setApiKeyValidity(false);
+            $this->pdkAccountRepository->store(null);
+            throw $e;
         }
+
+        $this->fillAccount($account);
+        $this->pdkAccountRepository->store($account);
+        $this->setApiKeyValidity(true);
+        Actions::execute(PdkBackendActions::UPDATE_SUBSCRIPTION_FEATURES);
     }
 }
