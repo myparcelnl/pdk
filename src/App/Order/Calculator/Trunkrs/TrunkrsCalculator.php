4730268731ef66c0dcab1601dad6ad2a7cc5ba29
jochemgravendeel <jochem.gravendeel@myparcel.nl>
2025-10-27 15:14:05 +0100

    Add signature

---
+++ src/App/Order/Calculator/Trunkrs/TrunkrsCalculator.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Pdk\App\Order\Calculator\Trunkrs;
+
+use MyParcelNL\Pdk\App\Order\Calculator\AbstractCarrierOptionsCalculator;
+
+final class TrunkrsCalculator extends AbstractCarrierOptionsCalculator
+{
+    protected function getCalculators(): array
+    {
+        return [
+            
+        ];
+    }
+}
