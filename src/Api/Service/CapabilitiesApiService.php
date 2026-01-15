aee5a336e37138469000960a7ebf7e01d944c29a
jochemgravendeel <jochem.gravendeel@myparcel.nl>
2026-01-15 16:19:50 +0100

    feat: set basic capabilities proxy

---
+++ src/Api/Service/CapabilitiesApiService.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyParcelNL\Pdk\Api\Service;
+
+use MyParcelNL\Pdk\Facade\Pdk;
+use MyParcelNL\Pdk\Facade\Settings;
+use MyParcelNL\Pdk\Settings\Model\AccountSettings;
+use RuntimeException;
+
+/**
+ * Service for communicating with the Capabilities API
+ */
+class CapabilitiesApiService extends AbstractApiService
+{
+    /**
+     * @return string
+     */
+    public function getBaseUrl(): string
+    {
+        $baseUrl = $this->baseUrl ?? Pdk::get('capabilitiesServiceUrl');
+
+        if (! $baseUrl) {
+            throw new RuntimeException('Capabilities service URL is not configured');
+        }
+
+        return $baseUrl;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHeaders(): array
+    {
+        $apiKey = Settings::get(AccountSettings::API_KEY, AccountSettings::ID);
+
+        if (! $apiKey) {
+            throw new RuntimeException('API key is not configured');
+        }
+
+        return [
+            'Authorization' => sprintf('bearer %s', base64_encode($apiKey)),
+            'User-Agent'    => $this->getUserAgentHeader(),
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    protected function getUserAgentHeader(): string
+    {
+        $userAgentStrings = [];
+        $userAgents       = array_merge(
+            (array) (Pdk::get('userAgent') ?? []),
+            [
+                'MyParcelNL-PDK' => Pdk::get('pdkVersion'),
+                'php'            => PHP_VERSION,
+            ]
+        );
+
+        foreach ($userAgents as $platform => $version) {
+            if ($version) {
+                $userAgentStrings[] = sprintf('%s/%s', $platform, $version);
+            }
+        }
+
+        return implode(' ', $userAgentStrings);
+    }
+}
