6d1d373e4186f6de05a928e74c3114fe6eb9e8bf
Joeri van Veen <joeri@myparcel.nl>
2026-01-22 13:47:31 +0100

    fix(v2): distribute weight when multiple shipments
    INT-1343

--- src/App/Order/Collection/PdkOrderCollection.php
+++ src/App/Order/Collection/PdkOrderCollection.php
@@ -54,10 +54,13 @@ class PdkOrderCollection extends Collection
 
             $schema->setCarrier($order->deliveryOptions->carrier);
 
-            $amountOfShipmentsToCreate = $schema->canHaveMultiCollo() ? 1 : $order->deliveryOptions->labelAmount;
+            $amountOfShipmentsToCreate = max(1, $schema->canHaveMultiCollo() ? 1 : $order->deliveryOptions->labelAmount);
 
             for ($i = 0; $i < $amountOfShipmentsToCreate; $i++) {
-                $newShipments->push($order->createShipment());
+                $newShipment = $order->createShipment();
+                // distribute weight equally over shipments (use original labelAmount so weight is exported correctly for multi-collo as well)
+                $newShipment->physicalProperties->weight /= $order->deliveryOptions->labelAmount;
+                $newShipments->push($newShipment);
             }
         });
 
