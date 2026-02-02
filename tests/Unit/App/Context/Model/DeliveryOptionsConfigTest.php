465ac8f8fb36e358d9296e1efe7a9b9b954b83d0
Nabil Azahaf <nabil.azahaf@myparcel.nl>
2026-02-02 10:31:04 +0100

    fix: use correct proposition constants

--- tests/Unit/App/Context/Model/DeliveryOptionsConfigTest.php
+++ tests/Unit/App/Context/Model/DeliveryOptionsConfigTest.php
@@ -11,6 +11,7 @@ use MyParcelNL\Pdk\App\Cart\Model\PdkCart;
 use MyParcelNL\Pdk\App\Order\Contract\PdkProductRepositoryInterface;
 use MyParcelNL\Pdk\Facade\Pdk;
 use MyParcelNL\Pdk\Facade\Settings;
+use MyParcelNL\Pdk\Proposition\Proposition;
 use MyParcelNL\Pdk\Settings\Model\CarrierSettings;
 use MyParcelNL\Pdk\Settings\Model\CheckoutSettings;
 use MyParcelNL\Pdk\Tests\Bootstrap\MockPdkProductRepository;
@@ -56,7 +57,7 @@ it('can be instantiated', function () {
             'packageType'                       => 'package',
             'pickupLocationsDefaultView'        => $pickupLocationsDefaultView,
             'allowPickupLocationsViewSelection' => $allowPickupLocationsViewSelection,
-            'proposition'                       => Platform::MYPARCEL_NAME,
+            'proposition'                       => Proposition::MYPARCEL_NAME,
             'platform'                          => Platform::LEGACY_MYPARCEL_NAME,
             'showPriceSurcharge'                => false,
             'priceStandardDelivery'             => \floatval(0),
@@ -189,7 +190,7 @@ it('uses correct price when price is shown as surcharge', function () {
             'allowPickupLocationsViewSelection' => true,
             'closedDays'                        => [],
             'excludeParcelLockers'              => false,
-            'proposition'                       => Platform::MYPARCEL_NAME,
+            'proposition'                       => Proposition::MYPARCEL_NAME,
         ]);
 });
 
