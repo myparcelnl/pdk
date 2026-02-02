05b5cbceb441ec7b87f2636b6adf53109c58e1b9
jochemgravendeel <jochem.gravendeel@myparcel.nl>
2026-02-02 10:21:47 +0100

    fix: prevent deprecation warnings for address1/address2 fields

--- tests/Unit/Base/Model/AddressTest.php
+++ tests/Unit/Base/Model/AddressTest.php
@@ -7,7 +7,9 @@ declare(strict_types=1);
 namespace MyParcelNL\Pdk\Base\Model;
 
 use MyParcelNL\Pdk\Base\Support\Utils;
+use MyParcelNL\Pdk\Facade\Pdk;
 use MyParcelNL\Pdk\Tests\Uses\UsesMockPdkInstance;
+use Psr\Log\LoggerInterface;
 
 use function MyParcelNL\Pdk\Tests\usesShared;
 
@@ -52,6 +54,33 @@ it('correctly transforms deprecated fields', function (array $input, array $outp
         'output' => [
             'street'        => 'street 123 b',
             'streetAdditionalInfo' => 'b',
+        ],
+    ],
+]);
+
+it('does not log deprecation warnings for address1 or address2', function (array $input) {
+    /** @var \MyParcelNL\Pdk\Tests\Bootstrap\MockLogger $logger */
+    $logger = Pdk::get(LoggerInterface::class);
+    $logger->clear();
+
+    new Address($input);
+
+    expect($logger->getLogs('notice'))->toBe([]);
+})->with([
+    'address1' => [
+        'input' => [
+            'address1' => 'Wegstraat 2F',
+        ],
+    ],
+    'address2' => [
+        'input' => [
+            'address2' => 'Wegstraat 2',
+        ],
+    ],
+    'address1 and address2' => [
+        'input' => [
+            'address1' => 'street 123',
+            'address2' => 'b',
         ],
     ],
 ]);
