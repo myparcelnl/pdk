ea198f69dc36547e5f0b5bebf7f375b855c40f2d
Freek van Rijt <freek.vanrijt@myparcel.nl>
2026-02-03 15:40:46 +0100

    feat(repository): allow for the PDK to know whether a PdkOrder actually exists
    introduce a new ModelRepositry that more closely follows the Repository pattern. It's intended for use in tandem with database-backed systems.
    
    It allows to actually tell whether a given order exists in the webshop instead of always populating a DTO-like model with whatever input is passed to `get`. See included deprecations and comments for future implementation details.
    
    This change should be non-breaking in its current form.

--- src/App/Order/Contract/PdkOrderRepositoryInterface.php
+++ src/App/Order/Contract/PdkOrderRepositoryInterface.php
@@ -6,12 +6,15 @@ namespace MyParcelNL\Pdk\App\Order\Contract;
 
 use MyParcelNL\Pdk\App\Order\Collection\PdkOrderCollection;
 use MyParcelNL\Pdk\App\Order\Model\PdkOrder;
+use MyParcelNL\Pdk\Base\Contract\ModelRepositoryInterface;
 use MyParcelNL\Pdk\Base\Contract\RepositoryInterface;
 
-interface PdkOrderRepositoryInterface extends RepositoryInterface
+interface PdkOrderRepositoryInterface extends ModelRepositoryInterface
 {
     /**
      * Create a new order object from input data.
+     *
+     * @deprecated this will be removed in a future release. Switch to instantiating PdkOrder directly if you want a new order. Use find() to retrieve an existing order.
      */
     public function get($input): PdkOrder;
 
@@ -19,6 +22,7 @@ interface PdkOrderRepositoryInterface extends RepositoryInterface
      * Create a collection of order objects from input data
      *
      * @param  string|string[] $orderIds - Single id, array of ids or string of semicolon-separated ids.
+     * @deprecated this will be removed in a future release. Switch to instantiating PdkOrderCollection directly. Or use `findAll()` to retrieve existing orders.
      */
     public function getMany($orderIds): PdkOrderCollection;
 
