38f39a1f7ee48450590e5d03579d7c1d8ecf512c
Freek van Rijt <freek.vanrijt@myparcel.nl>
2026-03-20 12:37:10 +0100

    style: fix typo

--- src/App/Webhook/Hook/ShipmentLabelCreatedWebhook.php
+++ src/App/Webhook/Hook/ShipmentLabelCreatedWebhook.php
@@ -19,7 +19,7 @@ final class ShipmentLabelCreatedWebhook extends AbstractHook
     public function handle(Request $request): void
     {
         // @TODO this webhook implementation below was broken, as the request payload for this webhook does not contain either "shipment_reference_identifier" or "shipment_id".
-        // This is an effective no-open to prevent unintended side-effects and since this webhook is always registered by the plugin, we cannot remove it easily.
+        // This is an effective no-op to prevent unintended side-effects and since this webhook is always registered by the plugin, we cannot remove it easily.
         // @SEE https://developer.myparcel.nl/api-reference/11.webhook-object-definitions.html#_11-c
         return;
         // $content = $this->getHookBody($request);
