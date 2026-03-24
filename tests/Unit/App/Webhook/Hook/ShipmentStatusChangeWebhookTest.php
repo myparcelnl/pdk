5bf503b3b96cb198156263ef0b829c55c4bcd771
Freek van Rijt <freek.vanrijt@myparcel.nl>
2026-03-24 16:04:28 +0100

    test: reset ShipmentStatusChangeWebhookTest to `main` version

--- tests/Unit/App/Webhook/Hook/ShipmentStatusChangeWebhookTest.php
+++ tests/Unit/App/Webhook/Hook/ShipmentStatusChangeWebhookTest.php
@@ -168,67 +168,17 @@ it('logs webhook received and processed', function () {
         })
         ->filter(fn(array $log) => in_array($log['message'], ['[PDK]: Webhook received', '[PDK]: Webhook processed']))
         ->values();
-    expect(
-        $logs->filter(function (array $log) {
-            return 0 === strpos($log['message'], '[PDK]: Update status');
-        })
-            ->first()['context']['status']
-    )
-        ->toBe($expectedStatus)
-        ->and($filteredLogs->toArray())
-        ->toBe([
-            [
-                'level'   => 'debug',
-                'message' => '[PDK]: Webhook received',
-                'context' => [],
-            ],
-            [
-                'level'   => 'debug',
-                'message' => '[PDK]: Webhook processed',
-                'context' => ['hook' => $expectedClass],
-            ],
-        ]);
-})->with([
-    'shipment updated' => [
-        'hook'   => WebhookSubscription::SHIPMENT_STATUS_CHANGE,
-        'class'  => ShipmentStatusChangeWebhook::class,
-        'status' => OrderSettings::STATUS_ON_LABEL_CREATE,
-        'body'   => [
-            'shipment_id'                   => 192031595,
-            'account_id'                    => 162450,
-            'order_id'                      => 'api-uuid-string',
-            'shop_id'                       => 83287,
-            'status'                        => 2,
-            'barcode'                       => '3SHOHR763563926',
-            'shipment_reference_identifier' => '',
+
+    expect($logs->toArray())->toBe([
+        [
+            'level'   => 'debug',
+            'message' => '[PDK]: Webhook received',
+            'context' => [],
         ],
-    ],
-    'shipment scanned' => [
-        'hook'   => WebhookSubscription::SHIPMENT_STATUS_CHANGE,
-        'class'  => ShipmentStatusChangeWebhook::class,
-        'status' => OrderSettings::STATUS_WHEN_LABEL_SCANNED,
-        'body'   => [
-            'shipment_id'                   => 192031595,
-            'account_id'                    => 162450,
-            'order_id'                      => 'api-uuid-string',
-            'shop_id'                       => 83287,
-            'status'                        => 5,
-            'barcode'                       => '3SHOHR763563926',
-            'shipment_reference_identifier' => '',
+        [
+            'level'   => 'debug',
+            'message' => '[PDK]: Webhook processed',
+            'context' => ['hook' => ShipmentStatusChangeWebhook::class],
         ],
-    ],
-    'shipment delivered' => [
-        'hook'   => WebhookSubscription::SHIPMENT_STATUS_CHANGE,
-        'class'  => ShipmentStatusChangeWebhook::class,
-        'status' => OrderSettings::STATUS_WHEN_DELIVERED,
-        'body'   => [
-            'shipment_id'                   => 192031595,
-            'account_id'                    => 162450,
-            'order_id'                      => 'api-uuid-string',
-            'shop_id'                       => 83287,
-            'status'                        => 9,
-            'barcode'                       => '3SHOHR763563926',
-            'shipment_reference_identifier' => '',
-        ],
-    ],
-]);
+    ]);
+});
