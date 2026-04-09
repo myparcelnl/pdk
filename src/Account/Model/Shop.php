904c314531af1c7754b7733b32f843bdeb04e395
Freek van Rijt <freek.vanrijt@myparcel.nl>
2026-04-09 12:38:35 +0200

    style: fix redundant extra whitespace

--- src/Account/Model/Shop.php
+++ src/Account/Model/Shop.php
@@ -12,48 +12,48 @@ use MyParcelNL\Pdk\Carrier\Collection\CarrierCollection;
 use MyParcelNL\Pdk\Carrier\Model\Carrier;
 
 /**
- * @property int                                $id
- * @property int                                $accountId
- * @property int                                $platformId
- * @property string                             $name
- * @property bool                               $hidden
- * @property array<string, mixed>               $billing
- * @property array<string, mixed>               $deliveryAddress
- * @property array<string, mixed>               $generalSettings
- * @property array<string, mixed>               $return
- * @property array<string, mixed>               $shipmentOptions
- * @property array<string, mixed>[]             $trackTrace
- * @property CarrierCollection                  $carriers
+ * @property int                    $id
+ * @property int                    $accountId
+ * @property int                    $platformId
+ * @property string                 $name
+ * @property bool                   $hidden
+ * @property array<string, mixed>   $billing
+ * @property array<string, mixed>   $deliveryAddress
+ * @property array<string, mixed>   $generalSettings
+ * @property array<string, mixed>   $return
+ * @property array<string, mixed>   $shipmentOptions
+ * @property array<string, mixed>[] $trackTrace
+ * @property CarrierCollection      $carriers
  */
 class Shop extends Model
 {
     public $attributes = [
-        'id'                    => null,
-        'accountId'             => null,
-        'platformId'            => null,
-        'name'                  => null,
-        'hidden'                => false,
-        'billing'               => [],
-        'deliveryAddress'       => [],
-        'generalSettings'       => [],
-        'return'                => [],
-        'shipmentOptions'       => [],
-        'trackTrace'            => [],
-        'carriers'              => CarrierCollection::class,
+        'id'              => null,
+        'accountId'       => null,
+        'platformId'      => null,
+        'name'            => null,
+        'hidden'          => false,
+        'billing'         => [],
+        'deliveryAddress' => [],
+        'generalSettings' => [],
+        'return'          => [],
+        'shipmentOptions' => [],
+        'trackTrace'      => [],
+        'carriers'        => CarrierCollection::class,
     ];
 
     protected $casts = [
-        'id'                    => 'int',
-        'accountId'             => 'int',
-        'platformId'            => 'int',
-        'name'                  => 'string',
-        'hidden'                => 'bool',
-        'billing'               => 'array',
-        'deliveryAddress'       => 'array',
-        'generalSettings'       => 'array',
-        'return'                => 'array',
-        'shipmentOptions'       => 'array',
-        'trackTrace'            => 'array',
-        'carriers'              => CarrierCollection::class,
+        'id'              => 'int',
+        'accountId'       => 'int',
+        'platformId'      => 'int',
+        'name'            => 'string',
+        'hidden'          => 'bool',
+        'billing'         => 'array',
+        'deliveryAddress' => 'array',
+        'generalSettings' => 'array',
+        'return'          => 'array',
+        'shipmentOptions' => 'array',
+        'trackTrace'      => 'array',
+        'carriers'        => CarrierCollection::class,
     ];
 }
